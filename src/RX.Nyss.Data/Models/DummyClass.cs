32562e42580a6a69a6333e6d941f86fe95b20749
Samson Valland <samson.valland@clave.no>
2019-10-10 11:24:09 +0200

    Created a dummy class to the context with a migration to check that it works as intended. Added documentation on how to create and run migrations.

---
+++ src/RX.Nyss.Data/Models/DummyClass.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace RX.Nyss.Data.Models
+{
+    // This is just a dummy class to test that migrations will work.
+    public class DummyClass
+    {
+        public int DummyColumn { get; set; }
+    }
+
+    public class DummyMap : IEntityTypeConfiguration<DummyClass>{
+        public void Configure(EntityTypeBuilder<DummyClass> builder) => builder.HasKey(d => d.DummyColumn);
+    }
+}
