c5a1693441a80e6feb12481987dd5ac103cd6ae1
Samson Valland <samson.valland@clave.no>
2019-10-10 15:45:52 +0200

    Added a simple Function with HttpTrigger just for pinging, and added startup with Configuration and Logging (serilog with AppInsights sink) setup.

---
+++ src/RX.Nyss.FuncApp/SmsReceiver.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Serilog;
+
+namespace RX.Nyss.FuncApp
+{
+    public class SmsReceiver
+    {
+        private readonly ILogger _logger;
+
+        public SmsReceiver(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        [FunctionName("Ping")]
+        public async Task<IActionResult> Ping(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ping")]
+            HttpRequestMessage req)
+        {
+            _logger.Debug("Received ping request");
+
+            return new OkObjectResult("I am alive!");
+        }
+    }
+}
---
+++ src/RX.Nyss.FuncApp/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RX.Nyss.FuncApp;
+using Serilog;
+using Serilog.Events;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace RX.Nyss.FuncApp
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.AddConfiguration();
+            builder.AddSerilog();
+        }
+    }
+
+    public static class FunctionHostBuilderExtensions
+    {
+        private const string LocalSettingsJsonFileName = "local.settings.json";
+
+        public static void AddConfiguration(this IFunctionsHostBuilder builder)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var localSettingsFile = Path.Combine(currentDirectory, LocalSettingsJsonFileName);
+
+            var provider = builder.Services.BuildServiceProvider();
+            var configuration = provider.GetService<IConfiguration>();
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddJsonFile(localSettingsFile, optional: true, reloadOnChange: true)
+                .AddConfiguration(configuration);
+            
+            var newConfiguration = configurationBuilder.Build();
+
+            builder.Services.AddSingleton<IConfiguration>(newConfiguration);
+        }
+
+        public static void AddSerilog(this IFunctionsHostBuilder builder)
+        {
+            var provider = builder.Services.BuildServiceProvider();
+            var configuration = provider.GetService<IConfiguration>();
+            var instrumentationKey = configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+
+            if (!Enum.TryParse(configuration["LogLevel"], true, out LogEventLevel minimumEventLevel))
+            {
+                minimumEventLevel = LogEventLevel.Information;
+            }
+
+            var loggerConfiguration = new LoggerConfiguration()
+                .MinimumLevel.Is(minimumEventLevel)
+                .Enrich.FromLogContext()
+                .WriteTo.Console();
+
+            if (!string.IsNullOrEmpty(instrumentationKey))
+            {
+                loggerConfiguration = loggerConfiguration.WriteTo.ApplicationInsights(TelemetryConfiguration.Active, TelemetryConverter.Traces);
+            }
+
+            var logger = loggerConfiguration.CreateLogger();
+
+            builder.Services.AddSingleton<ILogger>(logger);
+        }
+    }
+}
