452acd26465bee208283199e1310245893634cf6
Lukasz Inatlewski <lukasz.inatlewski@fabres.pl>
2019-10-17 13:06:25 +0200

    Updated logic of SmsReceiver, updated AppInsights configuration, updated function app configuration

--- src/RX.Nyss.FuncApp/SmsReceiver.cs
+++ src/RX.Nyss.FuncApp/SmsReceiver.cs
@@ -1,9 +1,11 @@
-﻿using System.Net.Http;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Serilog;
+using Flurl;
+using Flurl.Http;
 
 namespace RX.Nyss.FuncApp
 {
@@ -17,13 +19,40 @@ namespace RX.Nyss.FuncApp
         }
 
         [FunctionName("Ping")]
-        public async Task<IActionResult> Ping(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ping")]
-            HttpRequestMessage req)
+        public IActionResult Ping(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ping")] HttpRequestMessage httpRequest)
         {
             _logger.Debug("Received ping request");
-
             return new OkObjectResult("I am alive!");
+        }
+
+        [FunctionName("EnqueueReport")]
+        [return: ServiceBus("nrx-cbs-dev-nyss-bus-report-queue", Connection = "SERVICEBUS_CONNECTIONSTRING")]
+        public async Task<string> EnqueueReport(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "enqueueReport")] HttpRequestMessage httpRequest)
+        {
+            var httpRequestContent = await httpRequest.Content.ReadAsStringAsync();
+            _logger.Debug($"Received report: {httpRequestContent}");
+            return httpRequestContent;
+        }
+
+        [FunctionName("DequeueReport")]
+        public async Task DequeueReport(
+            [ServiceBusTrigger("nrx-cbs-dev-nyss-bus-report-queue", Connection = "SERVICEBUS_CONNECTIONSTRING")] string report)
+        {
+            _logger.Debug($"Dequeued report: {report}");
+
+            await "https://nrx-cbs-dev-nyss-funcapp.azurewebsites.net/"
+                .AppendPathSegments("api", "report")
+                .PostUrlEncodedAsync(report);
+        }
+
+        [FunctionName("Report")]
+        public async Task Report(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "report")] HttpRequestMessage httpRequest)
+        {
+            var httpRequestContent = await httpRequest.Content.ReadAsStringAsync();
+            _logger.Debug($"Report stored: {httpRequestContent}");
         }
     }
 }
