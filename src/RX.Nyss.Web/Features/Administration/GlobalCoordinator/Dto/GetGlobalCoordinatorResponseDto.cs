10ce99df0431fc76036617d1898c1ee566efb314
Przemysław Łabęcki <przemyslaw.labecki@fabres.pl>
2019-10-24 15:07:06 +0200

    Add Global Coordinator endpoints
    Update the endpoint for creation
    Add endpoints for listing, getting and edition
    Update and refactor related unit tests

---
+++ src/RX.Nyss.Web/Features/Administration/GlobalCoordinator/Dto/GetGlobalCoordinatorResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RX.Nyss.Web.Features.Administration.GlobalCoordinator.Dto
+{
+    public class GetGlobalCoordinatorsResponseDto
+    {
+        public int Id { get; set; }  
+        public string Name { get; set; }  
+        public string EmailAddress { get; set; }  
+        public string PhoneNumber { get; set; }  
+        public string Organization { get; set; }  
+    }
+}
--- src/RX.Nyss.Web/Features/UserVerification/Dto/VerifyAndStorePasswordRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RX.Nyss.Web.Features.UserVerification.Dto
-{
-    public class VerifyAndStorePasswordRequestDto
-    {
-        public string Email { get; set; }
-        public string Token { get; set; }
-        public string Password { get; set; }
-    }
-}
