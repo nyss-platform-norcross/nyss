2dd8e544560ce5588af9335ee56eda93f717829a
Przemysław Łabęcki <przemyslaw.labecki@fabres.pl>
2019-10-24 15:19:57 +0200

    Update global coordinator dto validation

--- src/RX.Nyss.Web/Features/Administration/GlobalCoordinator/Validators/EditGlobalCoordinatorValidator.cs
+++ src/RX.Nyss.Web/Features/Administration/GlobalCoordinator/Validators/EditGlobalCoordinatorValidator.cs
@@ -3,12 +3,12 @@ using RX.Nyss.Web.Features.Administration.GlobalCoordinator.Dto;
 
 namespace RX.Nyss.Web.Features.Administration.GlobalCoordinator.Validators
 {
-    public class RegisterGlobalCoordinatorValidator : AbstractValidator<RegisterGlobalCoordinatorRequestDto>
+    public class EditGlobalCoordinatorValidator : AbstractValidator<EditGlobalCoordinatorRequestDto>
     {
-        public RegisterGlobalCoordinatorValidator()
+        public EditGlobalCoordinatorValidator()
         {
+            RuleFor(x => x.Id).NotEmpty();
             RuleFor(x => x.Name).NotEmpty().MaximumLength(100);
-            RuleFor(x => x.Email).NotEmpty().MaximumLength(100).EmailAddress();
             RuleFor(x => x.PhoneNumber).NotEmpty().MaximumLength(20);
             RuleFor(x => x.Organization).MaximumLength(100);
         }
