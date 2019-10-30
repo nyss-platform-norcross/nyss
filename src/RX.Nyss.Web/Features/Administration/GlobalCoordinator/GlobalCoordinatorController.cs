8179a2164e1a8f45fb1734ff43e759317926f489
Samson Valland <samson.valland@clave.no>
2019-10-30 14:50:47 +0100

    Code review fixes: Refactor GlobalCoordinatorController
    - Move logic into GlobalCoordinatorService
    - Update test

--- src/RX.Nyss.Web/Features/Administration/GlobalCoordinator/GlobalCoordinatorController.cs
+++ src/RX.Nyss.Web/Features/Administration/GlobalCoordinator/GlobalCoordinatorController.cs
@@ -13,15 +13,11 @@ namespace RX.Nyss.Web.Features.Administration.GlobalCoordinator
 {
     public class GlobalCoordinatorController : BaseController
     {
-        private readonly IEmailPublisherService _emailPublisherService;
         private readonly IGlobalCoordinatorService _globalCoordinatorService;
-        private readonly IConfig _config;
 
-        public GlobalCoordinatorController(IGlobalCoordinatorService globalCoordinatorService, IEmailPublisherService emailPublisherService, IConfig config)
+        public GlobalCoordinatorController(IGlobalCoordinatorService globalCoordinatorService)
         {
             _globalCoordinatorService = globalCoordinatorService;
-            _emailPublisherService = emailPublisherService;
-            _config = config;
         }
 
         /// <summary>
@@ -31,26 +27,6 @@ namespace RX.Nyss.Web.Features.Administration.GlobalCoordinator
         /// <returns></returns>
         [HttpPost("registerGlobalCoordinator")]
         [NeedsRole(Role.Administrator)]
-        public async Task<Result> RegisterGlobalCoordinator([FromBody] RegisterGlobalCoordinatorRequestDto dto)
-        {
-            var (result, securityStamp) = await _globalCoordinatorService.RegisterGlobalCoordinator(dto);
-
-            if (!result.IsSuccess)
-            {
-                return result;
-            }
-
-            var baseUrl = new Uri(_config.BaseUrl);
-            var verificationUrl = new Uri(baseUrl, $"verifyEmail?email={WebUtility.UrlEncode(dto.Email)}&token={WebUtility.UrlEncode(securityStamp)}").ToString();
-
-            var (emailSubject, emailBody) = EmailTextGenerator.GenerateEmailVerificationEmail(
-                Role.GlobalCoordinator.ToString(),
-                verificationUrl,
-                dto.Name);
-
-            await _emailPublisherService.SendEmail((dto.Email, dto.Name), emailSubject, emailBody);
-
-            return result;
-        }
+        public async Task<Result> RegisterGlobalCoordinator([FromBody] RegisterGlobalCoordinatorRequestDto dto) => await _globalCoordinatorService.RegisterGlobalCoordinator(dto);
     }
 }
