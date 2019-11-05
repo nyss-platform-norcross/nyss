6c21be3e02c4214609f306128908f49c4bece7b1
Przemysław Łabęcki <przemyslaw.labecki@fabres.pl>
2019-11-05 08:04:33 +0100

    Refactor user access authorization handlers

--- src/RX.Nyss.Web/Features/Authentication/Policies/DataConsumerAccessHandler.cs
+++ src/RX.Nyss.Web/Features/Authentication/Policies/DataConsumerAccessHandler.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using RX.Nyss.Data.Models;
-using RX.Nyss.Web.Features.Authentication.Policies.BaseAccessHandlers;
 using RX.Nyss.Web.Services;
 
 namespace RX.Nyss.Web.Features.Authentication.Policies
@@ -11,23 +10,32 @@ namespace RX.Nyss.Web.Features.Authentication.Policies
     {
     }
 
-    public class DataConsumerAccessHandler : BaseUserAccessHandler<DataConsumerUser, DataConsumerAccessRequirement>
+    public class DataConsumerAccessHandler : AuthorizationHandler<DataConsumerAccessRequirement>
     {
         private const string RouteParameterName = "dataConsumerId";
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IResourceAccessService _resourceAccessService;
 
-        public DataConsumerAccessHandler(IHttpContextAccessor httpContextAccessor, INationalSocietyAccessService nationalSocietyAccessService)
-            :base(httpContextAccessor, nationalSocietyAccessService, RouteParameterName)
+        public DataConsumerAccessHandler(IHttpContextAccessor httpContextAccessor, IResourceAccessService resourceAccessService)
         {
+            _httpContextAccessor = httpContextAccessor;
+            _resourceAccessService = resourceAccessService;
         }
 
-        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context,
             DataConsumerAccessRequirement requirement)
         {
-            if (!context.User.Identity.IsAuthenticated)
+            var dataConsumerId = _httpContextAccessor.GetRouteParameterAsInt(RouteParameterName);
+            if (!context.User.Identity.IsAuthenticated || !dataConsumerId.HasValue)
             {
-                return Task.CompletedTask;
+                return;
             }
-            return HandleUserResourceRequirement(context, requirement);
+
+            var dataConsumerNationalSocieties = await _resourceAccessService.GetUserNationalSocietyIds<DataConsumerUser>(dataConsumerId.Value);
+            if (await _resourceAccessService.GetUserHasAccessToAnyOfResourceNationalSocieties(context.User, dataConsumerNationalSocieties))
+            {
+                context.Succeed(requirement);
+            }
         }
 
         
--- src/RX.Nyss.Web/Services/ResourceAccessService.cs
+++ src/RX.Nyss.Web/Services/ResourceAccessService.cs
@@ -2,48 +2,52 @@
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RX.Nyss.Data;
 using RX.Nyss.Data.Concepts;
+using RX.Nyss.Data.Models;
+using RX.Nyss.Web.Features.Authentication.Policies;
 
 namespace RX.Nyss.Web.Services
 {
-    public interface INationalSocietyAccessService
+    public interface IResourceAccessService
     {
-        Task<bool> HasAccessToAnyOfResourceNationalSocieties(ClaimsPrincipal user, List<int> resourceNationalSocietyIds);
-        Task<List<int>> GetUserNationalSocietyIds<T>(int userId) where T: Nyss.Data.Models.User;
+        Task<bool> GetUserHasAccessToAnyOfResourceNationalSocieties(ClaimsPrincipal userUnderAuthorization, List<int> resourceNationalSocietyIds);
+        Task<List<int>> GetUserNationalSocietyIds<T>(int userId) where T: User;
     }
 
-    public class NationalSocietyAccessService : INationalSocietyAccessService
+    public class ResourceAccessService : IResourceAccessService
     {
         private readonly IEnumerable<string> _rolesWithAccessToAllNationalSocieties;
         protected readonly INyssContext _nyssContext;
         protected readonly UserManager<IdentityUser> _userManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public NationalSocietyAccessService(INyssContext nyssContext, UserManager<IdentityUser> userManager)
+        public ResourceAccessService(INyssContext nyssContext, UserManager<IdentityUser> userManager, IHttpContextAccessor httpContextAccessor)
         {
             _nyssContext = nyssContext;
             _userManager = userManager;
+            _httpContextAccessor = httpContextAccessor;
             _rolesWithAccessToAllNationalSocieties = new List<Role> { Role.Administrator, Role.GlobalCoordinator }
                 .Select(role => role.ToString());
         }
 
-        public async Task<bool> HasAccessToAnyOfResourceNationalSocieties(ClaimsPrincipal user, List<int> resourceNationalSocietyIds)
+        public async Task<bool> GetUserHasAccessToAnyOfResourceNationalSocieties(ClaimsPrincipal userUnderAuthorization, List<int> resourceNationalSocietyIds)
         {
-            var userNationalSocietyIds = await GetUserNationalSocietyIds(user);
-            var hasAccessToAnyOfResourceNationalSocieties = resourceNationalSocietyIds.Intersect(userNationalSocietyIds).Any();
-            return hasAccessToAnyOfResourceNationalSocieties || HasAccessToAllNationalSocieties(user);
+            var authorizedUserNationalSocietyIds = await GetUserNationalSocietyIds(userUnderAuthorization);
+            var hasAccessToAnyOfResourceNationalSocieties = resourceNationalSocietyIds.Intersect(authorizedUserNationalSocietyIds).Any();
+            return hasAccessToAnyOfResourceNationalSocieties || HasAccessToAllNationalSocieties(userUnderAuthorization);
         }
 
-        public Task<List<int>> GetUserNationalSocietyIds<T>(int userId) where T: Nyss.Data.Models.User =>
+        public Task<List<int>> GetUserNationalSocietyIds<T>(int userId) where T: User =>
             _nyssContext.Users
                 .OfType<T>()
                 .Where(u => u.Id == userId)
                 .SelectMany(u => u.UserNationalSocieties)
                 .Select(uns => uns.NationalSocietyId)
                 .ToListAsync();
-
 
         private async Task<List<int>> GetUserNationalSocietyIds(ClaimsPrincipal user)
         {
