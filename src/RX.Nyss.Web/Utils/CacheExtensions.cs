fd9d503adbae7a334955e4c5e24a33df84683626
Krzysztof Jeske <krzysztof.jeske@fabres.pl>
2019-11-06 08:40:30 +0100

    Add InMemoryCache

---
+++ src/RX.Nyss.Web/Utils/CacheExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using RX.Nyss.Web.Services;
+using RX.Nyss.Web.Utils.DataContract;
+using static RX.Nyss.Web.Utils.DataContract.Result;
+
+namespace RX.Nyss.Web.Utils
+{
+    public static class CacheExtensions
+    {
+        public static async Task<TResult> GetCachedValue<TResult>(this IInMemoryCache memoryCache, string key, TimeSpan validFor, Func<Task<TResult>> value)
+            where TResult : class
+        {
+            var cachedResult = memoryCache.Get<TResult>(key);
+
+            if (cachedResult != null)
+            {
+                return cachedResult;
+            }
+
+            var result = await value();
+
+            if (result != null)
+            {
+                memoryCache.Store(key, result, validFor);
+            }
+
+            return result;
+        }
+
+        public static async Task<Result<TResult>> GetCachedResult<TResult>(this IInMemoryCache memoryCache, string key, TimeSpan validFor, Func<Task<Result<TResult>>> value)
+            where TResult : class
+        {
+            var cachedResult = memoryCache.Get<TResult>(key);
+
+            if (cachedResult != null)
+            {
+                return Success(cachedResult);
+            }
+
+            var result = await value();
+
+            if (result.IsSuccess && result.Value != null)
+            {
+                memoryCache.Store(key, result.Value, validFor);
+            }
+
+            return result;
+        }
+    }
+}
