705fcc9c93110e8aa92481fe0b64696b1f4f135e
Krzysztof Jeske <krzysztof.jeske@fabres.pl>
2019-11-06 08:42:50 +0100

    Add global exception handler

---
+++ src/RX.Nyss.Web/Configuration/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using RX.Nyss.Web.Utils.DataContract;
+using RX.Nyss.Web.Utils.Logging;
+
+namespace RX.Nyss.Web.Configuration
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void UseCustomExceptionHandler(this IApplicationBuilder app) =>
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        var logger = context.RequestServices.GetService(typeof(ILoggerAdapter)) as ILoggerAdapter;
+                        logger?.Error(contextFeature.Error, "There was an unhandled exception.");
+
+                        var result = Result.Error(ResultKey.UnexpectedError);
+
+                        var json = JsonSerializer.Serialize(result, new JsonSerializerOptions
+                        {
+                            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                        });
+
+                        await context.Response.WriteAsync(json);
+                    }
+                });
+            });
+    }
+}
