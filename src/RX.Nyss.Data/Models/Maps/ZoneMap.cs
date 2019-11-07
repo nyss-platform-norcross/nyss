dd29cdcbd2ede467786c8549f353e1f2ae4d1394
Przemysław Łabęcki <przemyslaw.labecki@fabres.pl>
2019-11-07 12:30:37 +0100

    Fix duplicate OnDelete in VillageMap and ZoneMap

--- src/RX.Nyss.Data/Models/Maps/ZoneMap.cs
+++ src/RX.Nyss.Data/Models/Maps/ZoneMap.cs
@@ -8,7 +8,7 @@ namespace RX.Nyss.Data.Models.Maps
         public void Configure(EntityTypeBuilder<Zone> builder)
         {
             builder.HasKey(x => x.Id);
-            builder.HasOne(x => x.Village).WithMany().IsRequired().OnDelete(DeleteBehavior.Restrict).OnDelete(DeleteBehavior.Restrict);
+            builder.HasOne(x => x.Village).WithMany().IsRequired().OnDelete(DeleteBehavior.Restrict);
             builder.Property(x => x.Name).HasMaxLength(100);
         }
     }
