aa0df77b2867395a6dc0c389ddd7c750af0a2864
Samson Valland <samson.valland@clave.no>
2019-11-20 15:44:46 +0100

    4 - Fix styling of project list
    - ..a bit more similar to the sketches

--- src/RX.Nyss.Web/ClientApp/src/components/projects/ProjectsList.js
+++ src/RX.Nyss.Web/ClientApp/src/components/projects/ProjectsList.js
@@ -10,6 +10,7 @@ import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import { Loading } from '../common/loading/Loading';
 import { strings, stringKeys } from '../../strings';
 import dayjs from "dayjs"
+import styles from "./ProjectsList.module.scss";
 
 export const ProjectsList = ({ isListFetching, isRemoving, goToEdition, goToDashboard, remove, list, nationalSocietyId }) => {
   if (isListFetching) {
@@ -20,64 +21,73 @@ export const ProjectsList = ({ isListFetching, isRemoving, goToEdition, goToDash
     <Grid container spacing={3} style={{ maxWidth: 1200 }}>
       {list.map(project => (
         <Grid key={`projectsListItem_${project.id}`} item xs={6}>
-          <Card key={project.id}>
+          <Card key={project.id} style={{ padding: "10px" }}>
             <CardContent>
-              <Typography variant="h3" gutterBottom>
+              <Typography variant="h3" gutterBottom style={{ marginBottom: "20px" }}>
                 {project.name}
               </Typography>
-              <Typography variant="h6" color="textSecondary" gutterBottom>
-                {project.state}
-              </Typography>
               <Grid container spacing={3}>
-                <Grid item container xs={6} direction="column" spacing={2}>
+                <Grid item container xs={6} direction="column" spacing={1}>
                   <Grid item>
-                    <Typography variant="h6">
-                      {project.totalReportCount}
-                    </Typography>
-                    <Typography variant="body1" color="textSecondary" gutterBottom>
-                      {strings(stringKeys.project.list.totalReportCount)}
-                    </Typography>
-                    <Typography variant="h6">
-                      {project.activeDataCollectorCount}
-                    </Typography>
-                    <Typography variant="body1" color="textSecondary" gutterBottom>
-                      {strings(stringKeys.project.list.activeDataCollectorCount)}
-                    </Typography>
-                    <Typography variant="h6">
-                      {dayjs(project.startDate).format("YYYY-MM-DD")}
-                    </Typography>
-                    <Typography variant="body1" color="textSecondary" gutterBottom>
-                      {strings(stringKeys.project.list.startDate)}
-                    </Typography>
+                    <div className={styles.keyPoint}>
+                      <Typography variant="h6">
+                        {project.totalReportCount}
+                      </Typography>
+                      <Typography variant="body1" color="textSecondary" gutterBottom>
+                        {strings(stringKeys.project.list.totalReportCount)}
+                      </Typography>
+                    </div>
+                    <div className={styles.keyPoint}>
+                      <Typography variant="h6">
+                        {project.activeDataCollectorCount}
+                      </Typography>
+                      <Typography variant="body1" color="textSecondary" gutterBottom>
+                        {strings(stringKeys.project.list.activeDataCollectorCount)}
+                      </Typography>
+                    </div>
+                    <div className={styles.keyPoint}>
+                      <Typography variant="h6">
+                        {dayjs(project.startDate).format("YYYY-MM-DD")}
+                      </Typography>
+                      <Typography variant="body1" color="textSecondary" gutterBottom>
+                        {strings(stringKeys.project.list.startDate)}
+                      </Typography>
+                    </div>
                   </Grid>
                 </Grid>
-                <Grid item container xs={6} direction="column" spacing={3}>
+                <Grid item container xs={6} direction="column" spacing={1}>
                   <Grid item>
-                    <Typography variant="h6">
-                      {project.escalatedAlertCount}
-                    </Typography>
-                    <Typography variant="body1" color="textSecondary" gutterBottom>
-                      {strings(stringKeys.project.list.escalatedAlertCount)}
-                    </Typography>
-                    <Typography variant="h6">
-                      {project.supervisorCount}
-                    </Typography>
-                    <Typography variant="body1" color="textSecondary" gutterBottom>
-                      {strings(stringKeys.project.list.supervisorCount)}
-                    </Typography>
-                    <Typography variant="h6">
-                      {project.endDate ? dayjs(project.endDate).format("YYYY-MM-DD") : strings(stringKeys.project.list.ongoing)}
-                    </Typography>
-                    <Typography variant="body1" color="textSecondary" gutterBottom>
-                      {strings(stringKeys.project.list.endDate)}
-                    </Typography>
+                    <div className={styles.keyPoint}>
+                      <Typography variant="h6">
+                        {project.escalatedAlertCount}
+                      </Typography>
+                      <Typography variant="body1" color="textSecondary" gutterBottom>
+                        {strings(stringKeys.project.list.escalatedAlertCount)}
+                      </Typography>
+                    </div>
+                    <div className={styles.keyPoint}>
+                      <Typography variant="h6">
+                        {project.supervisorCount}
+                      </Typography>
+                      <Typography variant="body1" color="textSecondary" gutterBottom>
+                        {strings(stringKeys.project.list.supervisorCount)}
+                      </Typography>
+                    </div>
+                    <div className={styles.keyPoint}>
+                      <Typography variant="h6">
+                        {project.endDate ? dayjs(project.endDate).format("YYYY-MM-DD") : strings(stringKeys.project.list.ongoing)}
+                      </Typography>
+                      <Typography variant="body1" color="textSecondary" gutterBottom>
+                        {strings(stringKeys.project.list.endDate)}
+                      </Typography>
+                    </div>
                   </Grid>
                 </Grid>
               </Grid>
             </CardContent>
             <CardActions>
-              <Grid container direction="row" justify="flex-end" alignItems="center">
-                <Grid item xs={4}>
+              <Grid container direction="row" justify="flex-end" alignItems="right">
+                <Grid item>
                   <Button onClick={() => goToDashboard(nationalSocietyId, project.id)} endIcon={<ArrowForwardIcon />}>{strings(stringKeys.project.list.open)}</Button>
                 </Grid>
               </Grid>
