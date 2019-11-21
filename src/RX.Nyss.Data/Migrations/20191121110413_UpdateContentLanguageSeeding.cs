2f5c54f75fa1714fe4f24291e0f8e437e69731d5
Przemysław Łabęcki <przemyslaw.labecki@fabres.pl>
2019-11-21 12:48:16 +0100

    Add report service unit tests + content language update migration

---
+++ src/RX.Nyss.Data/Migrations/20191121110413_UpdateContentLanguageSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RX.Nyss.Data.Migrations
+{
+    public partial class UpdateContentLanguageSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "nyss",
+                table: "ContentLanguages",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "LanguageCode",
+                value: "en");
+
+            migrationBuilder.UpdateData(
+                schema: "nyss",
+                table: "ContentLanguages",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "LanguageCode",
+                value: "fr");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "nyss",
+                table: "ContentLanguages",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "LanguageCode",
+                value: "EN");
+
+            migrationBuilder.UpdateData(
+                schema: "nyss",
+                table: "ContentLanguages",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "LanguageCode",
+                value: "FR");
+        }
+    }
+}
