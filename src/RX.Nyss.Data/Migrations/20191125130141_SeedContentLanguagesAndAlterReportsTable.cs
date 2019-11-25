f148a192c0146002d07d60036b04d41c3a51cb9f
Samson Valland <samson.valland@clave.no>
2019-11-25 14:05:23 +0100

    Code review: Combine migrations to one new one with timestamp newer than the newest one after rebase.

---
+++ src/RX.Nyss.Data/Migrations/20191125130141_SeedContentLanguagesAndAlterReportsTable.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RX.Nyss.Data.Migrations
+{
+    public partial class SeedContentLanguagesAndAlterReportsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectHealthRiskId",
+                schema: "nyss",
+                table: "Reports",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.InsertData(
+                schema: "nyss",
+                table: "ApplicationLanguages",
+                columns: new[] { "Id", "DisplayName", "LanguageCode" },
+                values: new object[,]
+                {
+                    { 1, "English", "en" },
+                    { 2, "Français", "fr" }
+                });
+
+            migrationBuilder.UpdateData(
+                schema: "nyss",
+                table: "ContentLanguages",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "LanguageCode",
+                value: "en");
+
+            migrationBuilder.UpdateData(
+                schema: "nyss",
+                table: "ContentLanguages",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "LanguageCode",
+                value: "fr");
+
+            migrationBuilder.UpdateData(
+                schema: "nyss",
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ApplicationLanguageId",
+                value: 1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "nyss",
+                table: "ApplicationLanguages",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                schema: "nyss",
+                table: "ApplicationLanguages",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectHealthRiskId",
+                schema: "nyss",
+                table: "Reports",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                schema: "nyss",
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ApplicationLanguageId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                schema: "nyss",
+                table: "ContentLanguages",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "LanguageCode",
+                value: "EN");
+
+            migrationBuilder.UpdateData(
+                schema: "nyss",
+                table: "ContentLanguages",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "LanguageCode",
+                value: "FR");
+
+            migrationBuilder.UpdateData(
+                schema: "nyss",
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ApplicationLanguageId",
+                value: null);
+        }
+    }
+}
