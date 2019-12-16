a43f9b2771dcbe8c993ea703281fb28dc16885f7
Krzysztof Jeske <krzysztof.jeske@gmail.com>
2019-12-16 09:13:10 +0100

    Add migration with indexes

--- src/RX.Nyss.Data/Models/Maps/AlertMap.cs
+++ src/RX.Nyss.Data/Models/Maps/AlertMap.cs
@@ -10,7 +10,7 @@ namespace RX.Nyss.Data.Models.Maps
             builder.HasKey(x => x.Id);
             builder.Property(x => x.CreatedAt).IsRequired();
             builder.Property(x => x.Comments).HasMaxLength(500);
-            builder.Property(x => x.Status).HasConversion<string>().IsRequired();
+            builder.Property(x => x.Status).HasConversion<string>().HasMaxLength(20).IsRequired();
             builder.HasOne(x => x.ProjectHealthRisk).WithMany(x => x.Alerts).IsRequired().OnDelete(DeleteBehavior.Restrict);
 
             builder.HasIndex(x => x.CreatedAt);
