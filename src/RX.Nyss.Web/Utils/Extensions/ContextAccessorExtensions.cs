8a05f7c7dba0044b70ef9d6e70745244f5807765
Przemysław Łabęcki <przemyslaw.labecki@fabres.pl>
2019-12-16 10:40:36 +0100

    Refactor ContextAccessorExtensions

--- src/RX.Nyss.Web/Utils/Extensions/ContextAccessorExtensions.cs
+++ src/RX.Nyss.Web/Utils/Extensions/ContextAccessorExtensions.cs
@@ -4,22 +4,8 @@ namespace RX.Nyss.Web.Utils.Extensions
 {
     public static class ContextAccessorExtensions
     {
-        public static int? GetResourceParameter(this IHttpContextAccessor httpContextAccessor, string parameterName)
-        {
-            var request = httpContextAccessor.HttpContext.Request;
-
-            if (request.RouteValues.ContainsKey(parameterName) && int.TryParse(request.RouteValues[parameterName].ToString(), out var idFromRoute))
-            {
-                return idFromRoute;
-            }
-
-            if (request.Query.ContainsKey(parameterName) && int.TryParse(request.Query[parameterName].ToString(), out var idFromQuery))
-            {
-                return idFromQuery;
-            }
-
-            return null;
-        }
+        public static int? GetResourceParameter(this IHttpContextAccessor httpContextAccessor, string parameterName) =>
+            httpContextAccessor.HttpContext.Request.GetResourceParameter(parameterName);
 
         public static int? GetResourceParameter(this HttpRequest request, string parameterName)
         {
