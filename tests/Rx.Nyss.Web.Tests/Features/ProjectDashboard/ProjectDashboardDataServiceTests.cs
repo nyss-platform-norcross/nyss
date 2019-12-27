a322d8a0d3ba2a817621bc82bd5cd2e6a5d8e6a1
Przemysław Łabęcki <przemyslaw.labecki@fabres.pl>
2019-12-27 16:04:57 +0100

    Update project dashboard unit tests

---
+++ tests/Rx.Nyss.Web.Tests/Features/ProjectDashboard/ProjectDashboardDataServiceTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using RX.Nyss.Data;
+using RX.Nyss.Data.Concepts;
+using RX.Nyss.Web.Features.ProjectDashboard;
+using RX.Nyss.Web.Features.ProjectDashboard.Dto;
+using RX.Nyss.Web.Utils;
+using Shouldly;
+using Xunit;
+
+namespace RX.Nyss.Web.Tests.Features.ProjectDashboard
+{
+    public class ProjectDashboardDataServiceTests
+    {
+        private readonly IProjectDashboardDataService _projectDashboardDataService;
+        private readonly IDateTimeProvider _dateTimeProvider;
+        private readonly INyssContext _nyssContext;
+        private readonly ProjectDashboardTestData _testData;
+
+        public ProjectDashboardDataServiceTests()
+        {
+            _dateTimeProvider = new DateTimeProvider();
+            _testData = new ProjectDashboardTestData(_dateTimeProvider);
+
+            _nyssContext = _testData.GetNyssContextMock();
+            _projectDashboardDataService = new ProjectDashboardDataService(_nyssContext,_dateTimeProvider);
+        }
+
+        [Theory]
+        [InlineData(true, 8, 4, 0)]
+        [InlineData(false, 24, 12, 0)]
+        public async Task GetSummaryData_FilterReportsOnlyOnTraining_ShouldShowAllReportsAccordingToTrainingStatus(bool isTraining, int expectedReportsCount, int expectedActiveCollectorsCount, int expectedInactiveCollectorsCount)
+        {
+            //arrange
+            var filters = new FiltersRequestDto
+            {
+                StartDate = new DateTime(2000,01,01),
+                EndDate = new DateTime(2100,01,01),
+                HealthRiskId = null,
+                Area = null,
+                IsTraining = isTraining,
+                GroupingType = FiltersRequestDto.GroupingTypeDto.Day,
+                ReportsType = FiltersRequestDto.ReportsTypeDto.All
+            };
+
+            //act
+            var summaryData = await _projectDashboardDataService.GetSummaryData(_testData.ProjectId, filters);
+
+            //assert
+            summaryData.ReportCount.ShouldBe(expectedReportsCount);
+            summaryData.ActiveDataCollectorCount.ShouldBe(expectedActiveCollectorsCount);
+            summaryData.InactiveDataCollectorCount.ShouldBe(expectedInactiveCollectorsCount);
+        }
+
+        [Theory]
+        [InlineData(DataCollectorType.Human, false, 16, 8, 0)]
+        [InlineData(DataCollectorType.CollectionPoint, false, 8, 4, 0)]
+        [InlineData(DataCollectorType.Human, true, 4, 2, 0)]
+        [InlineData(DataCollectorType.CollectionPoint, true, 4, 2, 0)]
+        public async Task GetSummaryData_FilterReportsOnDataCollectorTypeAndTrainingStatus_ShouldShowAllReportsAccordingToDataCollectorType(DataCollectorType dataCollectorType, bool isTraining, int expectedReportsCount, int expectedActiveCollectorsCount, int expectedInactiveCollectorsCount)
+        {
+            //arrange
+            var filters = new FiltersRequestDto
+            {
+                StartDate = new DateTime(2000, 01, 01),
+                EndDate = new DateTime(2100, 01, 01),
+                HealthRiskId = null,
+                Area = null,
+                IsTraining = isTraining,
+                GroupingType = FiltersRequestDto.GroupingTypeDto.Day,
+                ReportsType = dataCollectorType == DataCollectorType.Human
+                    ? FiltersRequestDto.ReportsTypeDto.DataCollector
+                    : FiltersRequestDto.ReportsTypeDto.DataCollectionPoint
+            };
+
+            //act
+            var summaryData = await _projectDashboardDataService.GetSummaryData(_testData.ProjectId, filters);
+
+            //assert
+            summaryData.ReportCount.ShouldBe(expectedReportsCount);
+            summaryData.ActiveDataCollectorCount.ShouldBe(expectedActiveCollectorsCount);
+            summaryData.InactiveDataCollectorCount.ShouldBe(expectedInactiveCollectorsCount);
+        }
+
+        [Theory]
+        [InlineData(1, true, 3, 3, 1)]
+        [InlineData(1, false, 8, 8, 4)]
+        [InlineData(2, true, 5, 4, 0)]
+        [InlineData(2, false, 16, 12, 0)]
+        public async Task GetSummaryData_FilterReportsOnHealthRisks_ShouldReturnCorrectNumbers(int healthRiskId, bool isTraining, int expectedReportsCount, int expectedActiveCollectorsCount, int expectedInactiveCollectorsCount)
+        {
+            //arrange
+            var filters = new FiltersRequestDto
+            {
+                StartDate = new DateTime(2000, 01, 01),
+                EndDate = new DateTime(2100, 01, 01),
+                HealthRiskId = healthRiskId,
+                Area = null,
+                IsTraining = isTraining,
+                GroupingType = FiltersRequestDto.GroupingTypeDto.Day,
+                ReportsType = FiltersRequestDto.ReportsTypeDto.All
+            };
+
+            //act
+            var summaryData = await _projectDashboardDataService.GetSummaryData(_testData.ProjectId, filters);
+
+            //assert
+            summaryData.ReportCount.ShouldBe(expectedReportsCount);
+            summaryData.ActiveDataCollectorCount.ShouldBe(expectedActiveCollectorsCount);
+            summaryData.InactiveDataCollectorCount.ShouldBe(expectedInactiveCollectorsCount);
+        }
+
+        [Theory]
+        [InlineData(1, false, 12, 6, 6)]
+        [InlineData(2, false, 12, 6, 6)]
+        [InlineData(1, true, 4, 2, 2)]
+        [InlineData(2, true, 4, 2, 2)]
+        public async Task GetSummaryData_FilterReportsOnRegion_ShouldReturnCorrectNumbers(int regionId, bool isTraining, int expectedReportsCount, int expectedActiveCollectorsCount, int expectedInactiveCollectorsCount)
+        {
+            //arrange
+            var filters = new FiltersRequestDto
+            {
+                StartDate = new DateTime(2000, 01, 01),
+                EndDate = new DateTime(2100, 01, 01),
+                HealthRiskId = null,
+                Area = new FiltersRequestDto.AreaDto { Id = 1, Type = FiltersRequestDto.AreaTypeDto.Region },
+                IsTraining = isTraining,
+                GroupingType = FiltersRequestDto.GroupingTypeDto.Day,
+                ReportsType = FiltersRequestDto.ReportsTypeDto.All
+            };
+
+            //act
+            var summaryData = await _projectDashboardDataService.GetSummaryData(_testData.ProjectId, filters);
+
+            //assert
+            summaryData.ReportCount.ShouldBe(expectedReportsCount);
+            summaryData.ActiveDataCollectorCount.ShouldBe(expectedActiveCollectorsCount);
+            summaryData.InactiveDataCollectorCount.ShouldBe(expectedInactiveCollectorsCount);
+        }
+    }
+}
---
+++ tests/Rx.Nyss.Web.Tests/Features/ProjectDashboard/ProjectDashboardTestData.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using MockQueryable.NSubstitute;
+using NSubstitute;
+using RX.Nyss.Data;
+using RX.Nyss.Data.Concepts;
+using RX.Nyss.Data.Models;
+using RX.Nyss.Web.Utils;
+
+namespace RX.Nyss.Web.Tests.Features.ProjectDashboard
+{
+    public class ProjectDashboardTestData
+    {
+        private readonly IDateTimeProvider _dateTimeProvider;
+        public int ProjectId { get; set; } = 1;
+
+        private static readonly DateTime BaseDate = new DateTime(2019, 01, 01);
+
+        public List<NationalSociety> NationalSocieties { get; set; }
+        public List<HealthRisk> HealthRisks { get; set; }
+        public List<AlertRule> AlertRules { get; set; }
+        public List<Project> Projects { get; set; }
+        public List<ProjectHealthRisk> ProjectHealthRisks { get; set; }
+        public List<DataCollector> DataCollectors { get; set; }
+        public List<Report> Reports { get; set; }
+        public List<User> Users { get; set; }
+        public List<SupervisorUserProject> SupervisorUserProjects { get; set; }
+        public List<UserNationalSociety> UserNationalSocieties { get; set; }
+        public List<Region> Regions { get; set; }
+        public List<District> Districts { get; set; }
+        public List<Village> Villages { get; set; }
+        public List<Zone> Zones { get; set; }
+
+        public INyssContext GetNyssContextMock()
+        {
+            var nyssContextMock = Substitute.For<INyssContext>();
+
+            var nationalSocietiesDbSet = NationalSocieties.AsQueryable().BuildMockDbSet();
+            var healthRisksDbSet = HealthRisks.AsQueryable().BuildMockDbSet();
+            var alertRulesDbSet = AlertRules.AsQueryable().BuildMockDbSet();
+            var projectsDbSet = Projects.AsQueryable().BuildMockDbSet();
+            var projectHealthRisksDbSet = ProjectHealthRisks.AsQueryable().BuildMockDbSet();
+            var dataCollectorsDbSet = DataCollectors.AsQueryable().BuildMockDbSet();
+            var reportsDbSet = Reports.AsQueryable().BuildMockDbSet();
+            var usersDbSet = Users.AsQueryable().BuildMockDbSet();
+            var supervisorUserProjectsDbSet = SupervisorUserProjects.AsQueryable().BuildMockDbSet();
+            var userNationalSocietiesDbSet = UserNationalSocieties.AsQueryable().BuildMockDbSet();
+            var regionsDbSet = Regions.AsQueryable().BuildMockDbSet();
+            var districtsDbSet = Districts.AsQueryable().BuildMockDbSet();
+            var villagesDbSet = Villages.AsQueryable().BuildMockDbSet();
+            var zonesDbSet = Zones.AsQueryable().BuildMockDbSet();
+
+            nyssContextMock.NationalSocieties.Returns(nationalSocietiesDbSet);
+            nyssContextMock.HealthRisks.Returns(healthRisksDbSet);
+            nyssContextMock.AlertRules.Returns(alertRulesDbSet);
+            nyssContextMock.Projects.Returns(projectsDbSet);
+            nyssContextMock.ProjectHealthRisks.Returns(projectHealthRisksDbSet);
+            nyssContextMock.DataCollectors.Returns(dataCollectorsDbSet);
+            nyssContextMock.Reports.Returns(reportsDbSet);
+            nyssContextMock.Users.Returns(usersDbSet);
+            nyssContextMock.SupervisorUserProjects.Returns(supervisorUserProjectsDbSet);
+            nyssContextMock.UserNationalSocieties.Returns(userNationalSocietiesDbSet);
+            nyssContextMock.Regions.Returns(regionsDbSet);
+            nyssContextMock.Districts.Returns(districtsDbSet);
+            nyssContextMock.Villages.Returns(villagesDbSet);
+            nyssContextMock.Zones.Returns(zonesDbSet);
+
+            return nyssContextMock;
+        }
+
+    public ProjectDashboardTestData(IDateTimeProvider dateTimeProvider)
+        {
+            _dateTimeProvider = dateTimeProvider;
+
+
+            NationalSocieties = new List<NationalSociety> { new NationalSociety { Id = 1 } };
+
+            Projects = new List<Project>
+            {
+                new Project { Id = ProjectId, NationalSocietyId = NationalSocieties[0].Id, NationalSociety = NationalSocieties[0]}
+            };
+
+            Users = new List<User>
+            {
+                new SupervisorUser { Id = 1 }
+            };
+
+            UserNationalSocieties = new List<UserNationalSociety>
+            {
+                new UserNationalSociety { NationalSociety = NationalSocieties[0], NationalSocietyId = NationalSocieties[0].Id, User = Users[0], UserId = Users[0].Id }
+            };
+
+            SupervisorUserProjects = new List<SupervisorUserProject>
+            {
+                new SupervisorUserProject{ SupervisorUser = (SupervisorUser)Users[0], SupervisorUserId = Users[0].Id, Project = Projects[0], ProjectId = Projects[0].Id }
+            };
+
+            AlertRules = new List<AlertRule>
+            {
+                new AlertRule{ Id = 1, CountThreshold = 1},
+                new AlertRule{ Id = 2, CountThreshold = 1}
+            };
+
+            HealthRisks = new List<HealthRisk>
+            {
+                new HealthRisk{Id = 1, AlertRule = AlertRules[0], HealthRiskType = HealthRiskType.Human },
+                new HealthRisk{Id = 2, AlertRule = AlertRules[1], HealthRiskType = HealthRiskType.Human }
+            };
+
+            ProjectHealthRisks = new List<ProjectHealthRisk>
+            {
+                new ProjectHealthRisk{Id=1, AlertRule = AlertRules[0], Project = Projects[0], HealthRisk = HealthRisks[0], HealthRiskId = HealthRisks[0].Id, Reports = new List<Report>() },
+                new ProjectHealthRisk{Id=2, AlertRule = AlertRules[1], Project = Projects[0], HealthRisk = HealthRisks[1], HealthRiskId = HealthRisks[1].Id, Reports = new List<Report>() }
+            };
+
+            GenerateGeographicalStructure();
+            GenerateDataCollectorsWithReports();
+        }
+
+        private void GenerateGeographicalStructure()
+        {
+            Regions = new List<Region>
+            {
+                new Region { Id = 1, Name = "Region 1", NationalSociety = NationalSocieties[0]},
+                new Region { Id = 2, Name = "Region 2", NationalSociety = NationalSocieties[0]}
+            };
+
+            Districts = new List<District>
+            {
+                new District {Id = 1, Name = "District 1", Region = Regions[0]},
+                new District {Id = 2, Name = "District 2", Region = Regions[0]},
+                new District {Id = 3, Name = "District 3", Region = Regions[1]},
+                new District {Id = 4, Name = "District 4", Region = Regions[1]}
+            };
+
+            Regions[0].Districts = Districts.GetRange(0,2);
+            Regions[1].Districts = Districts.GetRange(2,2);
+
+            Villages = new List<Village>
+            {
+                new Village{ Id = 1, District = Districts[0], Name = "Village 1" },
+                new Village{ Id = 2, District = Districts[0], Name = "Village 2" },
+                new Village{ Id = 3, District = Districts[1], Name = "Village 3" },
+                new Village{ Id = 4, District = Districts[1], Name = "Village 4" },
+                new Village{ Id = 5, District = Districts[2], Name = "Village 5" },
+                new Village{ Id = 6, District = Districts[2], Name = "Village 6" },
+                new Village{ Id = 7, District = Districts[3], Name = "Village 7" },
+                new Village{ Id = 8, District = Districts[3], Name = "Village 8" },
+            };
+
+            Districts[0].Villages = Villages.GetRange(0, 2);
+            Districts[1].Villages = Villages.GetRange(2, 2);
+            Districts[2].Villages = Villages.GetRange(4, 2);
+            Districts[3].Villages = Villages.GetRange(6, 2);
+
+            Zones = new List<Zone>
+            {
+                new Zone{ Id = 1, Village = Villages [0], Name = "Zone 1" },
+                new Zone{ Id = 2, Village = Villages [0], Name = "Zone 2" },
+                new Zone{ Id = 3, Village = Villages [1], Name = "Zone 3" },
+                new Zone{ Id = 4, Village = Villages [1], Name = "Zone 4" },
+                new Zone{ Id = 5, Village = Villages [2], Name = "Zone 5" },
+                new Zone{ Id = 6, Village = Villages [2], Name = "Zone 6" },
+                new Zone{ Id = 7, Village = Villages [3], Name = "Zone 7" },
+                new Zone{ Id = 8, Village = Villages [3], Name = "Zone 8" },
+                new Zone{ Id = 9, Village = Villages [4], Name = "Zone 9" },
+                new Zone{ Id = 10, Village = Villages [4], Name = "Zone 10" },
+                new Zone{ Id = 11, Village = Villages [5], Name = "Zone 11" },
+                new Zone{ Id = 12, Village = Villages [5], Name = "Zone 12" },
+                new Zone{ Id = 13, Village = Villages [6], Name = "Zone 13" },
+                new Zone{ Id = 14, Village = Villages [6], Name = "Zone 14" },
+                new Zone{ Id = 15, Village = Villages [7], Name = "Zone 15" },
+                new Zone{ Id = 16, Village = Villages [7], Name = "Zone 16" },
+            };
+
+            Villages[0].Zones = Zones.GetRange(0, 2);
+            Villages[1].Zones = Zones.GetRange(2, 2);
+            Villages[2].Zones = Zones.GetRange(4, 2);
+            Villages[3].Zones = Zones.GetRange(6, 2);
+            Villages[4].Zones = Zones.GetRange(8, 2);
+            Villages[5].Zones = Zones.GetRange(10, 2);
+            Villages[6].Zones = Zones.GetRange(12, 2);
+            Villages[7].Zones = Zones.GetRange(14, 2);
+        }
+
+
+        private void GenerateDataCollectorsWithReports()
+        {
+            var numberOfDataCollectors = 16;
+            var numberOfHumanDataCollectors = 10;
+            var numberOfDataCollectionPoints = 6;
+            var humansStartIndex = 0;
+            var collectionPointsStartIndex = numberOfHumanDataCollectors;
+            var numberOfTrainingHumans = 2;
+            var numberOfTrainingCollectionPoints = 2;
+            var numberOfReports = numberOfDataCollectors * 2;
+
+
+            DataCollectors = Enumerable.Range(1, numberOfDataCollectors)
+                .Select(i => new DataCollector
+                {
+                    Id = i,
+                    Project = Projects[0],
+                    Supervisor = (SupervisorUser)Users[0],
+                    DataCollectorType = DataCollectorType.Human,
+                    IsInTrainingMode = false,
+                    Reports = new List<Report>(),
+                    Zone = Zones [i-1],
+                    Village = Zones[i-1].Village
+                })
+                .ToList();
+
+            DataCollectors.GetRange(collectionPointsStartIndex, numberOfDataCollectionPoints).ForEach(dc => dc.DataCollectorType = DataCollectorType.CollectionPoint);
+            DataCollectors.GetRange(humansStartIndex, numberOfTrainingHumans).ForEach(dc => dc.IsInTrainingMode = true);
+            DataCollectors.GetRange(collectionPointsStartIndex, numberOfTrainingCollectionPoints).ForEach(dc => dc.IsInTrainingMode = true);
+
+            Reports = Enumerable.Range(1, numberOfReports)
+                .Select(i => new Report
+                {
+                    Id = i,
+                    DataCollector = DataCollectors[(i-1)/2],
+                    Status = ReportStatus.New,
+                })
+                .ToList();
+
+            Reports.ForEach(r =>
+            {
+                r.IsTraining = r.DataCollector.IsInTrainingMode;
+                r.Zone = r.DataCollector.Zone;
+                r.Village = r.DataCollector.Village;
+                r.CreatedAt = BaseDate.AddDays(r.Id);
+                r.ReceivedAt = r.CreatedAt;
+                r.EpiWeek = _dateTimeProvider.GetEpiWeek(r.CreatedAt);
+                r.ProjectHealthRisk = ProjectHealthRisks[ (((r.Id-1) % 3) == 0) ? 0 : 1];
+                r.RawReport = new RawReport { Id = r.Id, DataCollector = r.DataCollector, NationalSociety = NationalSocieties[0], IsTraining = r.IsTraining };
+
+                r.DataCollector.Reports.Add(r);
+                r.ProjectHealthRisk.Reports.Add(r);
+
+                r.Zone = r.DataCollector.Zone;
+                r.Village = r.DataCollector.Village;
+            });
+
+            Reports.Where( r => r.DataCollector.DataCollectorType == DataCollectorType.Human).ToList()
+                .ForEach(r =>
+                    r.ReportedCase = (r.Id % 4) switch
+                    {
+                        0 => new ReportCase { CountMalesBelowFive = 1, CountMalesAtLeastFive = 0, CountFemalesBelowFive = 0, CountFemalesAtLeastFive = 0 },
+                        1 => new ReportCase { CountMalesAtLeastFive = 1, CountFemalesBelowFive = 0, CountFemalesAtLeastFive = 0, CountMalesBelowFive = 0 },
+                        2 => new ReportCase { CountFemalesBelowFive = 1, CountMalesBelowFive = 0, CountMalesAtLeastFive = 0, CountFemalesAtLeastFive = 0 },
+                        3 => new ReportCase { CountFemalesAtLeastFive = 1, CountMalesBelowFive = 0, CountFemalesBelowFive = 0, CountMalesAtLeastFive = 0 },
+                        _ => r.ReportedCase
+                    });
+
+            Reports.Where(r => r.DataCollector.DataCollectorType == DataCollectorType.CollectionPoint).ToList()
+                .ForEach(r =>
+                {
+                    r.ReportType = ReportType.DataCollectionPoint;
+                    r.ReportedCase = (r.Id % 4) switch
+                    {
+                        0 => new ReportCase { CountMalesBelowFive = 1, CountMalesAtLeastFive = 0, CountFemalesBelowFive = 0, CountFemalesAtLeastFive = 0 },
+                        1 => new ReportCase { CountMalesAtLeastFive = 1, CountFemalesBelowFive = 0, CountFemalesAtLeastFive = 0, CountMalesBelowFive = 0 },
+                        2 => new ReportCase { CountFemalesBelowFive = 1, CountMalesBelowFive = 0, CountMalesAtLeastFive = 0, CountFemalesAtLeastFive = 0 },
+                        3 => new ReportCase { CountFemalesAtLeastFive = 1, CountMalesBelowFive = 0, CountFemalesBelowFive = 0, CountMalesAtLeastFive = 0 },
+                        _ => r.ReportedCase
+                    };
+                    r.DataCollectionPointCase = (r.Id % 3) switch
+                    {
+                        0 => new DataCollectionPointCase { DeathCount = 1, FromOtherVillagesCount = 0, ReferredCount = 0 },
+                        1 => new DataCollectionPointCase { FromOtherVillagesCount = 1, ReferredCount = 0, DeathCount = 0 },
+                        2 => new DataCollectionPointCase { ReferredCount = 1, FromOtherVillagesCount = 0, DeathCount = 0 },
+                        _ => r.DataCollectionPointCase
+                    };
+                });
+        }
+    }
+}
