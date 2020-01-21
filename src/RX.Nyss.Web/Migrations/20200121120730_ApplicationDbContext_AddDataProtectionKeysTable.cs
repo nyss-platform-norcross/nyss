44c11d4229b1cbb2697dd0030d25f2607338b187
Lukasz Inatlewski <lukasz.inatlewski@fabres.pl>
2020-01-21 13:10:45 +0100

    Share data protection keys
    - Use SQL Server to share data protection keys
    - Upgrade NuGet packages

---
+++ src/RX.Nyss.Web/Migrations/20200121120730_ApplicationDbContext_AddDataProtectionKeysTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RX.Nyss.Web.Migrations
+{
+    public partial class ApplicationDbContext_AddDataProtectionKeysTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DataProtectionKeys",
+                schema: "identity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FriendlyName = table.Column<string>(nullable: true),
+                    Xml = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataProtectionKeys", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataProtectionKeys",
+                schema: "identity");
+        }
+    }
+}
