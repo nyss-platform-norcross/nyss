e80b207baeb2e7e88a5d50bcdf1fcc608e018aeb
Gard Njåtun Krøyer <gard.njatun.kroyer@redcross.no>
2020-01-31 09:51:59 +0100

    Only validate english fields when creating health risk

--- src/RX.Nyss.Web/ClientApp/src/components/healthRisks/HealthRisksCreatePage.js
+++ src/RX.Nyss.Web/ClientApp/src/components/healthRisks/HealthRisksCreatePage.js
@@ -46,12 +46,14 @@ const HealthRisksCreatePageComponent = (props) => {
         [`contentLanguage_${lang.id}_caseDefinition`]: "",
         [`contentLanguage_${lang.id}_feedbackMessage`]: ""
       },
-      validation: {
+      validation: lang.name.toLowerCase() === "english"
+      ? {
         ...result.validation,
         [`contentLanguage_${lang.id}_name`]: [validators.required, validators.maxLength(100)],
         [`contentLanguage_${lang.id}_caseDefinition`]: [validators.required, validators.maxLength(500)],
         [`contentLanguage_${lang.id}_feedbackMessage`]: [validators.required, validators.maxLength(160)]
       }
+      : result.validation
     }), { fields, validation });
 
     return createForm(finalFormData.fields, finalFormData.validation);
