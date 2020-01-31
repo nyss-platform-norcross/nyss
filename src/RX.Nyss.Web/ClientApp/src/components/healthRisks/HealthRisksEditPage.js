41828bdbb09a217f830436a8ecfac54b2139b7a8
Gard Njåtun Krøyer <gard.njatun.kroyer@redcross.no>
2020-01-31 10:32:04 +0100

    Validate only english when editing health risk as well

--- src/RX.Nyss.Web/ClientApp/src/components/healthRisks/HealthRisksEditPage.js
+++ src/RX.Nyss.Web/ClientApp/src/components/healthRisks/HealthRisksEditPage.js
@@ -57,12 +57,14 @@ const HealthRisksEditPageComponent = (props) => {
           [`contentLanguage_${lang.id}_caseDefinition`]: content && content.caseDefinition,
           [`contentLanguage_${lang.id}_feedbackMessage`]: content && content.feedbackMessage
         },
-        validation: {
+        validation: lang.name.toLowerCase() === "english"
+        ? {
           ...result.validation,
           [`contentLanguage_${lang.id}_name`]: [validators.required, validators.maxLength(100)],
           [`contentLanguage_${lang.id}_caseDefinition`]: [validators.required, validators.maxLength(500)],
           [`contentLanguage_${lang.id}_feedbackMessage`]: [validators.required, validators.maxLength(160)]
         }
+        : result.validation
       }), { fields, validation });
 
     setForm(createForm(finalFormData.fields, finalFormData.validation));
