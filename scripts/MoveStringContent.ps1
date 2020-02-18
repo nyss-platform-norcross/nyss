4ce396d9c4497ead9bd2ed274b11cda71b808dd2
Samson Valland <samson.valland@clave.no>
2020-02-18 09:40:49 +0100

    Update script to also download the old blobs to make it more convenient for comparison
    - Also added the downloaded blobs' tmp dirs to our .gitignore

--- scripts/MoveStringContent.ps1
+++ scripts/MoveStringContent.ps1
@@ -1,14 +1,16 @@
 # A simple script that will:
 # 1. Snapshot the blobs in the "to" container
-# 2. Download the blobs in the "from" container
-# 3. Batch upload the downloaded blobs to the "to" container
+# 2. Download the new blobs in the "from" container
+# 3. Download the old blobs in the "to" container
+# 4. Batch upload the downloaded blobs to the "to" container
 param(
   [string] $from = "dev",
   [string] $to = "test"
 )
 
 $ScriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition;
-$folderForBlobs = Join-Path $ScriptPath "/tmp" -Resolve
+$folderForNewBlobs = "$ScriptPath\\tmp-new"
+$folderForOldBlobs = "$ScriptPath\\tmp-old"
 
 Write-Host "1. Snapshotting blobs that exist in the destination $to..."
 $blobs = az storage blob list --container-name nyss-blob-container --account-name nrxcbsnyssst$to | ConvertFrom-Json
@@ -17,10 +19,15 @@ $blobs | ForEach-Object {
   Write-Host "Snapshot of" $snapResult.name "last modified" $snapResult.properties.lastModified "created"
 }
 
-Write-Host "2. Download all blobs from source $from to $folderForBlobs"
-Remove-Item $folderForBlobs -Recurse -ErrorAction Ignore
-New-Item -Path $folderForBlobs -ItemType Directory
-az storage blob download-batch --destination $folderForBlobs --source nyss-blob-container --account-name nrxcbsnyssst$from
+Write-Host "2. Download all blobs from source $from to $folderForNewBlobs"
+Remove-Item $folderForNewBlobs -Recurse -ErrorAction Ignore
+New-Item -Path $folderForNewBlobs -ItemType Directory
+az storage blob download-batch --destination $folderForNewBlobs --source nyss-blob-container --account-name nrxcbsnyssst$from
 
-Write-Host "3. Uploading downloaded blobs to $to..."
-az storage blob upload-batch --source $folderForBlobs --destination nyss-blob-container --account-name nrxcbsnyssst$to
+Write-Host "3. Download old blobs from $to to $folderForOldBlobs"
+Remove-Item $folderForOldBlobs -Recurse -ErrorAction Ignore
+New-Item -Path $folderForOldBlobs -ItemType Directory
+az storage blob download-batch --destination $folderForOldBlobs --source nyss-blob-container --account-name nrxcbsnyssst$to
+
+Write-Host "4. Uploading downloaded blobs to $to..."
+az storage blob upload-batch --source $folderForNewBlobs --destination nyss-blob-container --account-name nrxcbsnyssst$to
