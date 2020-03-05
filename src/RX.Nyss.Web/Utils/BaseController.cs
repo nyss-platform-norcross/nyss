cf3228fa7a21790f15ded3967bf2f338a2bae87c
Samson Valland <samson.valland@clave.no>
2020-03-05 11:29:57 +0100

    [Patch] Add a ResultFilter and add it all endpoints that then sets the HttpContext.Response.StatusCode to 400 when the Response is of our Result type and has IsSuccess set to false, in order to more easily monitor bad requests in AppInsights etc.

--- src/RX.Nyss.Web/Utils/BaseController.cs
+++ src/RX.Nyss.Web/Utils/BaseController.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using RX.Nyss.Web.Utils.Filters;
 
 namespace RX.Nyss.Web.Utils
 {
     [ApiController]
     [Authorize]
+    [SetCorrectResponseCode]
     public class BaseController : ControllerBase
     {
     }
