02fa983a00c687b45ce003e6c9ade1d6f28de3cc
Samson Valland <samson.valland@clave.no>
2020-05-29 12:22:51 +0200

    Add extension method that should still be there

--- src/RX.Nyss.Common/Utils/StringExtensions.cs
+++ src/RX.Nyss.Common/Utils/StringExtensions.cs
@@ -7,5 +7,11 @@ namespace RX.Nyss.Common.Utils
         public static int? ParseToNullableInt(this string value) => int.TryParse(value, out var outValue)
             ? (int?)outValue
             : null;
+
+        public static string SubstringFromEnd(this string s, int numberOfCharacters)
+        {
+            var maxNumberOfChars = Math.Min(s.Length, numberOfCharacters);
+            return s.Substring(s.Length - maxNumberOfChars, maxNumberOfChars);
+        }
     }
 }
