cdf6d5e769b495580f9f38cb181eaeb45eb37daa
gardnk <gard.njatun.kroyer@redcross.no>
2021-02-16 14:28:02 +0100

    Make relationships between users and modems optional

---
+++ src/RX.Nyss.Data/Migrations/20210216131825_MakeUserModemRelationOptional.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RX.Nyss.Data.Migrations
+{
+    public partial class MakeUserModemRelationOptional : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- src/RX.Nyss.Data/Models/ManagerUser.cs
+++ src/RX.Nyss.Data/Models/ManagerUser.cs
@@ -2,7 +2,7 @@
 {
     public class ManagerUser : User
     {
-        public int ModemId { get; set; }
+        public int? ModemId { get; set; }
         public virtual GatewayModem Modem { get; set; }
     }
 }
