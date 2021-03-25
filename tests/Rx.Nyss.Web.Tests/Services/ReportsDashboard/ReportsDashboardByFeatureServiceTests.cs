01cb14f112ebeec028e4f5b23c0659de230fe41b
gardnk <gard.njatun.kroyer@redcross.no>
2021-03-25 09:47:24 +0100

    Add unit test for sex & age count in project dashboard

--- tests/Rx.Nyss.Web.Tests/Services/ReportsDashboard/ReportsDashboardByFeatureServiceTests.cs
+++ tests/Rx.Nyss.Web.Tests/Services/ReportsDashboard/ReportsDashboardByFeatureServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using NSubstitute;
 using RX.Nyss.Common.Utils;
@@ -13,10 +14,11 @@ namespace RX.Nyss.Web.Tests.Services.ReportsDashboard
 {
     public class ReportsDashboardByFeatureServiceTests
     {
-        private readonly ReportsDashboardByVillageService _reportsDashboardByDataCollectionPointService;
+        private readonly ReportsDashboardByVillageService _reportsDashboardByVillageService;
         private readonly INyssContext _nyssContext;
         private readonly INyssWebConfig _config;
         private readonly IReportService _reportService;
+        private readonly IReportsDashboardByFeatureService _reportsDashboardByFeatureService;
 
         public ReportsDashboardByFeatureServiceTests()
         {
@@ -27,11 +29,12 @@ namespace RX.Nyss.Web.Tests.Services.ReportsDashboard
             _config = Substitute.For<INyssWebConfig>();
 
             _reportService = Substitute.For<IReportService>();
-            _reportsDashboardByDataCollectionPointService = new ReportsDashboardByVillageService(_reportService, dateTimeProvider, _config);
+            _reportsDashboardByVillageService = new ReportsDashboardByVillageService(_reportService, dateTimeProvider, _config);
+            _reportsDashboardByFeatureService = new ReportsDashboardByFeatureService(_reportService, dateTimeProvider);
         }
 
         [Fact]
-        public async Task GetReportsGroupedByFeaturesAndDate_ShouldTakeOnlyMostActiveVillages()
+        public async Task GetReportsGroupedByVillageAndDate_ShouldTakeOnlyMostActiveVillages()
         {
             var reports = _nyssContext.Reports;
             _reportService.GetDashboardHealthRiskEventReportsQuery(Arg.Any<ReportsFilter>()).Returns(reports);
@@ -41,11 +44,44 @@ namespace RX.Nyss.Web.Tests.Services.ReportsDashboard
             _config.View.Returns(new ConfigSingleton.ViewOptions { NumberOfGroupedVillagesInProjectDashboard = numberOfGroupedVillagesInProjectDashboard });
 
             //act
-            var result = await _reportsDashboardByDataCollectionPointService.GetReportsGroupedByVillageAndDate(new ReportsFilter(), DatesGroupingType.Day);
+            var result = await _reportsDashboardByVillageService.GetReportsGroupedByVillageAndDate(new ReportsFilter(), DatesGroupingType.Day);
 
             //assert
             result.Villages.Count().ShouldBe(numberOfGroupedVillagesInProjectDashboard + 1);
             result.Villages.ElementAt(numberOfGroupedVillagesInProjectDashboard).Name.ShouldBe("(rest)");
+        }
+
+        [Fact]
+        public async Task GetReportsGroupedByFeaturesAndDate_ShouldReturnCorrectCount()
+        {
+            // arrange
+            var reports = _nyssContext.Reports;
+            _reportService.GetDashboardHealthRiskEventReportsQuery(Arg.Any<ReportsFilter>()).Returns(reports);
+            var startDate = new DateTime(2018, 11, 28);
+            var endDate = new DateTime(2019, 01, 01);
+
+            // act
+            var res = await _reportsDashboardByFeatureService.GetReportsGroupedByFeaturesAndDate(new ReportsFilter
+            {
+                StartDate = startDate,
+                EndDate = endDate
+            }, DatesGroupingType.Day);
+
+            var total = res
+                .Select(r => r.CountFemalesBelowFive + r.CountUnspecifiedSexAndAge + r.CountMalesBelowFive + r.CountFemalesAtLeastFive + r.CountMalesAtLeastFive)
+                .Aggregate(0, (a, c) => a + c);
+            var countMaleBelowFive = res.Sum(r => r.CountMalesBelowFive);
+            var countMaleAboveFive = res.Sum(r => r.CountMalesAtLeastFive);
+            var countFemaleBelowFive = res.Sum(r => r.CountFemalesBelowFive);
+            var countFemaleAboveFive = res.Sum(r => r.CountFemalesAtLeastFive);
+            var countUnspecified = res.Sum(r => r.CountUnspecifiedSexAndAge);
+
+            total.ShouldBe(32);
+            countMaleBelowFive.ShouldBe(7);
+            countMaleAboveFive.ShouldBe(7);
+            countFemaleBelowFive.ShouldBe(7);
+            countFemaleAboveFive.ShouldBe(7);
+            countUnspecified.ShouldBe(4);
         }
     }
 }
