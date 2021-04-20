80dddc80f7f7e49af74d35bb9c27eda64ca57449
Nikolai Riedel <29171805+nikoduesentrieb@users.noreply.github.com>
2021-04-20 15:59:25 +0200

    Add report id to report export

--- src/RX.Nyss.Web/Services/ExcelExportService.cs
+++ src/RX.Nyss.Web/Services/ExcelExportService.cs
@@ -66,65 +66,66 @@ namespace RX.Nyss.Web.Services
             {
                 var data = (ExportReportListResponseDto)reportListResponseDto;
                 var columnIndex = columnData.IndexOf(data) + 2;
-                worksheet.Cells[columnIndex, 1].Value = data.DateTime;
-                worksheet.Cells[columnIndex, 1].Style.Numberformat.Format = "yyyy-MM-dd";
+                worksheet.Cells[columnIndex, 1].Value = data.Id;
                 worksheet.Cells[columnIndex, 2].Value = data.DateTime;
-                worksheet.Cells[columnIndex, 2].Style.Numberformat.Format = "HH:mm";
-                worksheet.Cells[columnIndex, 3].Value = data.EpiYear;
-                worksheet.Cells[columnIndex, 4].Value = data.EpiWeek;
-                worksheet.Cells[columnIndex, 5].Value = data.Status;
-                worksheet.Cells[columnIndex, 6].Value = data.Region;
-                worksheet.Cells[columnIndex, 7].Value = data.District;
-                worksheet.Cells[columnIndex, 8].Value = data.Village;
-                worksheet.Cells[columnIndex, 9].Value = data.Zone;
-                worksheet.Cells[columnIndex, 10].Value = data.HealthRiskName;
-                worksheet.Cells[columnIndex, 11].Value = data.CountMalesBelowFive;
-                worksheet.Cells[columnIndex, 12].Value = data.CountMalesAtLeastFive;
-                worksheet.Cells[columnIndex, 13].Value = data.CountFemalesBelowFive;
-                worksheet.Cells[columnIndex, 14].Value = data.CountFemalesAtLeastFive;
-                worksheet.Cells[columnIndex, 15].Value = data.CountMalesBelowFive + data.CountFemalesBelowFive;
-                worksheet.Cells[columnIndex, 16].Value = data.CountMalesAtLeastFive + data.CountFemalesAtLeastFive;
-                worksheet.Cells[columnIndex, 17].Value = data.CountMalesBelowFive + data.CountMalesAtLeastFive;
-                worksheet.Cells[columnIndex, 18].Value = data.CountFemalesBelowFive + data.CountFemalesAtLeastFive;
-                worksheet.Cells[columnIndex, 19].Value = data.CountMalesBelowFive + data.CountMalesAtLeastFive + data.CountFemalesBelowFive + data.CountFemalesAtLeastFive;
+                worksheet.Cells[columnIndex, 2].Style.Numberformat.Format = "yyyy-MM-dd";
+                worksheet.Cells[columnIndex, 3].Value = data.DateTime;
+                worksheet.Cells[columnIndex, 3].Style.Numberformat.Format = "HH:mm";
+                worksheet.Cells[columnIndex, 4].Value = data.EpiYear;
+                worksheet.Cells[columnIndex, 5].Value = data.EpiWeek;
+                worksheet.Cells[columnIndex, 6].Value = data.Status;
+                worksheet.Cells[columnIndex, 7].Value = data.Region;
+                worksheet.Cells[columnIndex, 8].Value = data.District;
+                worksheet.Cells[columnIndex, 9].Value = data.Village;
+                worksheet.Cells[columnIndex, 10].Value = data.Zone;
+                worksheet.Cells[columnIndex, 11].Value = data.HealthRiskName;
+                worksheet.Cells[columnIndex, 12].Value = data.CountMalesBelowFive;
+                worksheet.Cells[columnIndex, 13].Value = data.CountMalesAtLeastFive;
+                worksheet.Cells[columnIndex, 14].Value = data.CountFemalesBelowFive;
+                worksheet.Cells[columnIndex, 15].Value = data.CountFemalesAtLeastFive;
+                worksheet.Cells[columnIndex, 16].Value = data.CountMalesBelowFive + data.CountFemalesBelowFive;
+                worksheet.Cells[columnIndex, 17].Value = data.CountMalesAtLeastFive + data.CountFemalesAtLeastFive;
+                worksheet.Cells[columnIndex, 18].Value = data.CountMalesBelowFive + data.CountMalesAtLeastFive;
+                worksheet.Cells[columnIndex, 19].Value = data.CountFemalesBelowFive + data.CountFemalesAtLeastFive;
+                worksheet.Cells[columnIndex, 20].Value = data.CountMalesBelowFive + data.CountMalesAtLeastFive + data.CountFemalesBelowFive + data.CountFemalesAtLeastFive;
 
                 if (reportListType == ReportListType.FromDcp)
                 {
-                    worksheet.Cells[columnIndex, 20].Value = data.ReferredCount;
-                    worksheet.Cells[columnIndex, 21].Value = data.DeathCount;
-                    worksheet.Cells[columnIndex, 22].Value = data.FromOtherVillagesCount;
-                    worksheet.Cells[columnIndex, 23].Value = data.DataCollectorDisplayName;
-                    worksheet.Cells[columnIndex, 24].Value = data.PhoneNumber;
-                    worksheet.Cells[columnIndex, 25].Value = data.Message;
-                    worksheet.Cells[columnIndex, 26].Value = data.Location != null ? $"{data.Location.Y}/{data.Location.X}" : "";
+                    worksheet.Cells[columnIndex, 21].Value = data.ReferredCount;
+                    worksheet.Cells[columnIndex, 22].Value = data.DeathCount;
+                    worksheet.Cells[columnIndex, 23].Value = data.FromOtherVillagesCount;
+                    worksheet.Cells[columnIndex, 24].Value = data.DataCollectorDisplayName;
+                    worksheet.Cells[columnIndex, 25].Value = data.PhoneNumber;
+                    worksheet.Cells[columnIndex, 26].Value = data.Message;
+                    worksheet.Cells[columnIndex, 27].Value = data.Location != null ? $"{data.Location.Y}/{data.Location.X}" : "";
                 }
                 else
                 {
-                    worksheet.Cells[columnIndex, 20].Value = data.DataCollectorDisplayName;
-                    worksheet.Cells[columnIndex, 21].Value = data.PhoneNumber;
-                    worksheet.Cells[columnIndex, 22].Value = data.Message;
-                    worksheet.Cells[columnIndex, 23].Value = data.ReportStatus;
-                    worksheet.Cells[columnIndex, 24].Value = data.ReportAlertId;
-                    worksheet.Cells[columnIndex, 25].Value = data.Location != null ? $"{data.Location.Y}/{data.Location.X}" : "";
+                    worksheet.Cells[columnIndex, 21].Value = data.DataCollectorDisplayName;
+                    worksheet.Cells[columnIndex, 22].Value = data.PhoneNumber;
+                    worksheet.Cells[columnIndex, 23].Value = data.Message;
+                    worksheet.Cells[columnIndex, 24].Value = data.ReportStatus;
+                    worksheet.Cells[columnIndex, 25].Value = data.ReportAlertId;
+                    worksheet.Cells[columnIndex, 26].Value = data.Location != null ? $"{data.Location.Y}/{data.Location.X}" : "";
                 }
             }
-            worksheet.Column(1).Width = 12; //Date
-            worksheet.Column(10).Width = 20; //HealthRiskName
+            worksheet.Column(2).Width = 12; //Date
+            worksheet.Column(11).Width = 20; //HealthRiskName
 
             if (reportListType == ReportListType.FromDcp)
             {
-                worksheet.Column(23).Width = 20; //DcName
-                worksheet.Column(24).Width = 20; //PhoneNr
-                worksheet.Column(25).Width = 20; //Message
-                worksheet.Column(26).Width = 37; //Location
+                worksheet.Column(24).Width = 20; //DcpName
+                worksheet.Column(25).Width = 20; //PhoneNr
+                worksheet.Column(26).Width = 20; //Message
+                worksheet.Column(27).Width = 37; //Location
             }
             else
             {
-                worksheet.Column(20).Width = 20; //DcName
-                worksheet.Column(21).Width = 20; //PhoneNr
-                worksheet.Column(22).Width = 12; //Message
-                worksheet.Column(23).Width = 14; //ReportStatus
-                worksheet.Column(25).Width = 37; //Location
+                worksheet.Column(21).Width = 20; //DcName
+                worksheet.Column(22).Width = 20; //PhoneNr
+                worksheet.Column(23).Width = 12; //Message
+                worksheet.Column(24).Width = 14; //ReportStatus
+                worksheet.Column(26).Width = 37; //Location
             }
 
             return package;
