a941e32188ecba07d2d3b98fb1744e38ec2afc7f
gardnk <gard.njatun.kroyer@redcross.no>
2021-05-07 11:55:06 +0200

    fix bug in updating data collector location

--- src/RX.Nyss.Web/ClientApp/src/components/dataCollectors/logic/dataCollectorsService.js
+++ src/RX.Nyss.Web/ClientApp/src/components/dataCollectors/logic/dataCollectorsService.js
@@ -29,8 +29,8 @@ export const getSaveFormModel = (values, type, locations) =>
     deployed: values.deployed,
     locations: locations.map(location => ({
       id: location.id || null,
-      latitude: values[`location_${location.number}_latitude`],
-      longitude: values[`location_${location.number}_longitude`],
+      latitude: parseFloat(values[`location_${location.number}_latitude`]),
+      longitude: parseFloat(values[`location_${location.number}_longitude`]),
       regionId: parseInt(values[`location_${location.number}_regionId`]),
       districtId: parseInt(values[`location_${location.number}_districtId`]),
       villageId: parseInt(values[`location_${location.number}_villageId`]),
