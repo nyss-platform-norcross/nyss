2e43e10111e20316455579261afd201371f17f26
Mai Amundsen <mai.amundsen@redcross.no>
2021-06-13 17:22:07 +0200

    Rename date field in log table

--- src/RX.Nyss.Data/Models/AlertEventLog.cs
+++ src/RX.Nyss.Data/Models/AlertEventLog.cs
@@ -11,11 +11,10 @@ namespace RX.Nyss.Data.Models
         public int? AlertEventSubtypeId { get; set; }
         public int LoggedById { get; set; }
         public string Textfield { get; set; }
-        public DateTime DateTime { get; set; }
+        public DateTime CreatedAt { get; set; }
         public virtual User LoggedBy { get; set; }
         public virtual Alert Alert { get; set; }
         public virtual AlertEventType AlertEventType { get; set; }
         public virtual AlertEventSubtype AlertEventSubtype { get; set; }
-
     }
 }
