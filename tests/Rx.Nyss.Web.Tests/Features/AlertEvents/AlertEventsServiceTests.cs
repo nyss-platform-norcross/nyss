4238b6541a1718fdc256b7f2f7825561a5fbbb12
Mai Amundsen <mai.amundsen@redcross.no>
2021-06-24 20:06:02 +0200

    Add tests for AlertEventsService - 88% coverage!

--- tests/Rx.Nyss.Web.Tests/Features/AlertEvents/AlertEventsServiceTests.cs
+++ tests/Rx.Nyss.Web.Tests/Features/AlertEvents/AlertEventsServiceTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using MockQueryable.NSubstitute;
 using NSubstitute;
+using RX.Nyss.Common.Utils.DataContract;
 using RX.Nyss.Data;
 using RX.Nyss.Data.Concepts;
 using RX.Nyss.Data.Models;
@@ -62,6 +63,52 @@ namespace RX.Nyss.Web.Tests.Features.AlertEvents
                 }
             };
 
+            var eventTypes = new List<AlertEventType>
+            {
+                new AlertEventType
+                {
+                    Id = TestData.AlertEventTypeInvestigationId,
+                    Name = TestData.AlertEventTypeInvestigation,
+                    AlertEventSubtypes = new List<AlertEventSubtype>
+                    {
+                        new AlertEventSubtype
+                        {
+                            Id = TestData.AlertEventSubtypeInvestigatedId,
+                            Name = TestData.AlertEventSubtypeInvestigated,
+                            AlertEventTypeId = TestData.AlertEventTypeInvestigationId
+                        },
+                        new AlertEventSubtype
+                        {
+                            Id = TestData.AlertEventSubtypeNotInvestigatedId,
+                            Name = TestData.AlertEventSubtypeNotInvestigated,
+                            AlertEventTypeId = TestData.AlertEventTypeInvestigationId
+                        }
+                    }
+                },
+                new AlertEventType
+                {
+                    Id = TestData.AlertEventTypeDetailsId,
+                    Name = TestData.AlertEventTypeDetails,
+                    AlertEventSubtypes = new AlertEventSubtype[] {},
+                }
+            };
+
+            var eventSubtypes = new List<AlertEventSubtype>
+            {
+                new AlertEventSubtype
+                {
+                    Id = TestData.AlertEventSubtypeInvestigatedId,
+                    Name = TestData.AlertEventSubtypeInvestigated,
+                    AlertEventTypeId = TestData.AlertEventTypeInvestigationId
+                },
+                new AlertEventSubtype
+                {
+                    Id = TestData.AlertEventSubtypeNotInvestigatedId,
+                    Name = TestData.AlertEventSubtypeNotInvestigated,
+                    AlertEventTypeId = TestData.AlertEventTypeInvestigationId
+                }
+            };
+
             var nationalSocietiesDbSet = nationalSocieties.AsQueryable()
                 .BuildMockDbSet();
             _nyssContext.NationalSocieties.Returns(nationalSocietiesDbSet);
@@ -69,6 +116,14 @@ namespace RX.Nyss.Web.Tests.Features.AlertEvents
             var unsDbSet = userNationalSociety.AsQueryable()
                 .BuildMockDbSet();
             _nyssContext.UserNationalSocieties.Returns(unsDbSet);
+
+            var eventTypeDbSet = eventTypes.AsQueryable()
+                .BuildMockDbSet();
+            _nyssContext.AlertEventTypes.Returns(eventTypeDbSet);
+
+            var eventSubtypeDbSet = eventSubtypes.AsQueryable()
+                .BuildMockDbSet();
+            _nyssContext.AlertEventSubtypes.Returns(eventSubtypeDbSet);
 
             _alerts = TestData.GetAlerts(nationalSocieties[0]);
             var alertsDbSet = _alerts.AsQueryable()
@@ -82,17 +137,21 @@ namespace RX.Nyss.Web.Tests.Features.AlertEvents
 
             _authorizationService.GetCurrentUser()
                 .Returns(_currentUser);
-
         }
 
+        #region GetEventLog Tests
 
         [Fact]
         public async Task GetAlertEventLogs_ShouldOrderItemsByDate()
         {
-            var reportAcceptedAt = new DateTime(2020, 1, 5);
+            var reportAcceptedAt = new DateTime(2020, 1, 6);
+            var reportResetAt = new DateTime(2020, 1, 7);
+            var reportRejectedAt = new DateTime(2020, 1, 8);
+
             var escalatedAt = new DateTime(2020, 1, 10);
             var closedAt = new DateTime(2020, 1, 15);
-            var notInvestigatedAt = new DateTime(2021, 6, 22);
+
+            var investigatedAt = new DateTime(2021, 6, 22, 22, 30, 00);
             var detailsRegisteredAt = new DateTime(2021, 6, 23);
 
             _alerts.First().Status = AlertStatus.Closed;
@@ -105,16 +164,26 @@ namespace RX.Nyss.Web.Tests.Features.AlertEvents
             _alerts.First().AlertReports.First().Report.AcceptedAt = reportAcceptedAt;
             _alerts.First().AlertReports.First().Report.AcceptedBy = _currentUser;
 
+            _alerts.First().AlertReports.First().Report.Status = ReportStatus.Pending;
+            _alerts.First().AlertReports.First().Report.ResetAt = reportResetAt;
+            _alerts.First().AlertReports.First().Report.ResetBy = _currentUser;
+
+            _alerts.First().AlertReports.First().Report.Status = ReportStatus.Rejected;
+            _alerts.First().AlertReports.First().Report.RejectedAt = reportRejectedAt;
+            _alerts.First().AlertReports.First().Report.RejectedBy = _currentUser;
+
             var result = await _alertEventsService.GetLogItems(TestData.AlertId, 1);
 
             result.IsSuccess.ShouldBeTrue();
-            result.Value.LogItems.Count().ShouldBe(6);
+            result.Value.LogItems.Count().ShouldBe(8);
             result.Value.LogItems.ElementAt(0).Date.ShouldBe(TestData.AlertCreatedAt.AddHours(1));
             result.Value.LogItems.ElementAt(1).Date.ShouldBe(reportAcceptedAt.AddHours(1));
-            result.Value.LogItems.ElementAt(2).Date.ShouldBe(escalatedAt.AddHours(1));
-            result.Value.LogItems.ElementAt(3).Date.ShouldBe(closedAt.AddHours(1));
-            result.Value.LogItems.ElementAt(4).Date.ShouldBe(notInvestigatedAt.AddHours(1));
-            result.Value.LogItems.ElementAt(5).Date.ShouldBe(detailsRegisteredAt.AddHours(1));
+            result.Value.LogItems.ElementAt(2).Date.ShouldBe(reportResetAt.AddHours(1));
+            result.Value.LogItems.ElementAt(3).Date.ShouldBe(reportRejectedAt.AddHours(1));
+            result.Value.LogItems.ElementAt(4).Date.ShouldBe(escalatedAt.AddHours(1));
+            result.Value.LogItems.ElementAt(5).Date.ShouldBe(closedAt.AddHours(1));
+            result.Value.LogItems.ElementAt(6).Date.ShouldBe(investigatedAt.AddHours(1));
+            result.Value.LogItems.ElementAt(7).Date.ShouldBe(detailsRegisteredAt.AddHours(1));
         }
 
 
@@ -224,6 +293,145 @@ namespace RX.Nyss.Web.Tests.Features.AlertEvents
             result.Value.LogItems.ElementAt(1).LoggedBy.ShouldBe(userName);
         }
 
+        #endregion
+
+        #region Create Tests
+
+        [Fact]
+        public async Task CreateAlertEventLogItem_ShouldReturnSuccess()
+        {
+            var logItem = new CreateAlertEventRequestDto
+            {
+                Timestamp = TestData.Event1CreatedAt,
+                EventTypeId = 1,
+                EventSubtypeId = 1,
+                Text = "The doctor found nothing wrong.",
+                UtcOffset = 1,
+            };
+
+            await _alertEventsService.CreateAlertEventLogItem(alertId: TestData.AlertId, createDto: logItem);
+
+            await _nyssContext.Received(1)
+                .AddAsync(Arg.Any<AlertEventLog>());
+        }
+
+        [Fact]
+        public void CreateAlertEventLogItem_WhenEventTypeIsMissing_ShouldReturnSuccess()
+        {
+            var logItem = new CreateAlertEventRequestDto
+            {
+                Timestamp = TestData.Event1CreatedAt,
+                EventTypeId = 0,
+                EventSubtypeId = TestData.AlertEventSubtypeNotInvestigatedId,
+                Text = "My event type is missing.",
+                UtcOffset = 1,
+            };
+
+            Should.ThrowAsync<Exception>(() => _alertEventsService.CreateAlertEventLogItem(TestData.AlertId, logItem));
+        }
+
+        [Fact]
+        public async Task CreateAlertEventLogItem_WhenEventTypeDoesntHaveSubtype_ShouldReturnSuccess()
+        {
+            var logItem = new CreateAlertEventRequestDto
+            {
+                Timestamp = TestData.Event1CreatedAt,
+                EventTypeId = TestData.AlertEventTypeDetailsId,
+                EventSubtypeId = null,
+                Text = "My name is details and I dont need subtypes.",
+                UtcOffset = 1,
+            };
+
+            await _alertEventsService.CreateAlertEventLogItem(alertId: TestData.AlertId, createDto: logItem);
+
+            await _nyssContext.Received(1)
+                .AddAsync(Arg.Any<AlertEventLog>());
+        }
+
+
+        [Fact]
+        public void CreateAlertEvent_WhenEventTypeDoesntExist_ShouldThrowException()
+        {
+            var logItem = new CreateAlertEventRequestDto
+            {
+                Timestamp = TestData.Event1CreatedAt,
+                EventTypeId = 3,
+                EventSubtypeId = TestData.AlertEventSubtypeInvestigatedId,
+                Text = "Event type 3 doesnt exist.",
+                UtcOffset = 1,
+            };
+
+            Should.ThrowAsync<Exception>(() => _alertEventsService.CreateAlertEventLogItem(TestData.AlertId, logItem));
+        }
+
+        [Fact]
+        public void CreateAlertEvent_WhenSubtypeIsNotValid_ShouldThrowException()
+        {
+            var logItem = new CreateAlertEventRequestDto
+            {
+                Timestamp = TestData.Event1CreatedAt,
+                EventTypeId = TestData.AlertEventTypeDetailsId,
+                EventSubtypeId = TestData.AlertEventSubtypeInvestigatedId,
+                Text = "Details doesnt have a subtype.",
+                UtcOffset = 1,
+            };
+
+            Should.ThrowAsync<Exception>(() => _alertEventsService.CreateAlertEventLogItem(TestData.AlertId, logItem));
+        }
+
+        #endregion
+
+        #region Edit Tests
+
+        [Fact]
+        public async Task EditAlertEvent_WhenTextIsEdited_ShouldReturnSuccess()
+        {
+            var logItem = new EditAlertEventRequestDto
+            {
+                AlertEventLogId = 1,
+                Text = "I do exist."
+            };
+
+            var result = await _alertEventsService.EditAlertEventLogItem(logItem);
+
+            result.IsSuccess.ShouldBeTrue();
+            result.Message.Key.ShouldBe(ResultKey.AlertEvent.EditSuccess);
+        }
+
+
+        [Fact]
+        public void EditAlertEvent_WhenLogItemDoesntExist_ShouldThrowException()
+        {
+            var logItem = new EditAlertEventRequestDto
+            {
+                AlertEventLogId = 4,
+                Text = "I do not exist."
+            };
+
+            Should.ThrowAsync<Exception>(() => _alertEventsService.EditAlertEventLogItem(logItem));
+        }
+
+        #endregion
+
+        #region Delete Tests
+
+
+        [Fact]
+        public async Task DeleteAlertEvent_ShouldReturnSuccess()
+        {
+            var result = await _alertEventsService.DeleteAlertEventLogItem(1);
+
+            result.IsSuccess.ShouldBeTrue();
+            result.Message.Key.ShouldBe(ResultKey.AlertEvent.DeleteSuccess);
+        }
+
+        [Fact]
+        public void DeleteAlertEvent_WhenLogItemIdDoesntExist_ShouldThrowException() =>
+            Should.ThrowAsync<Exception>(() => _alertEventsService.DeleteAlertEventLogItem(5));
+
+        #endregion
+
+
         internal static class TestData
         {
             public const int AlertId = 1;
@@ -234,14 +442,19 @@ namespace RX.Nyss.Web.Tests.Features.AlertEvents
             public static readonly DateTime AlertCreatedAt = new DateTime(2020, 1, 5);
             public static readonly ManagerUser DefaultUser = new ManagerUser() { Name = "DefaultManager" };
 
-            public const int AlertEventTypeId = 1;
-            public const int AlertEventSubtypeId = 2;
-            public const string AlertEventTypeDetails = "Details";
+            public const int AlertEventTypeInvestigationId = 1;
             public const string AlertEventTypeInvestigation = "Investigation";
+
+            public const int AlertEventSubtypeInvestigatedId = 1;
             public const string AlertEventSubtypeInvestigated = "Investigated";
+
+            public const int AlertEventSubtypeNotInvestigatedId = 2;
             public const string AlertEventSubtypeNotInvestigated = "NotInvestigated";
 
-            public static readonly DateTime Event1CreatedAt = new DateTime(2021, 6, 22);
+            public const int AlertEventTypeDetailsId = 2;
+            public const string AlertEventTypeDetails = "Details";
+
+            public static readonly DateTime Event1CreatedAt = new DateTime(2021, 6, 22, 22, 30, 00);
             public static readonly DateTime Event2CreatedAt = new DateTime(2021, 6, 23);
 
             public static List<Alert> GetAlerts(NationalSociety nationalSociety) =>
@@ -323,9 +536,15 @@ namespace RX.Nyss.Web.Tests.Features.AlertEvents
                     {
                         Id = 1,
                         AlertId = AlertId,
-                        AlertEventType = new AlertEventType { Name = AlertEventTypeInvestigation, Id = AlertEventTypeId},
-                        AlertEventSubtype = new AlertEventSubtype { Name = AlertEventSubtypeInvestigated, AlertEventTypeId = AlertEventTypeId},
-                        Text = "The case was not investigated.",
+                        AlertEventType = new AlertEventType
+                        {
+                            Name = AlertEventTypeInvestigation, Id = AlertEventTypeInvestigationId
+                        },
+                        AlertEventSubtype = new AlertEventSubtype
+                        {
+                            Name = AlertEventSubtypeInvestigated, AlertEventTypeId = AlertEventSubtypeInvestigatedId
+                        },
+                        Text = "The case was investigated.",
                         CreatedAt = Event1CreatedAt,
                         LoggedBy = DefaultUser
                     },
