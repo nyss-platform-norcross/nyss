3e060bb1db3e3bf1d87a11cc4fd86acbcef65155
Mai Amundsen <mai.amundsen@redcross.no>
2021-09-08 17:35:31 +0200

    Hide status for activity reports

--- src/RX.Nyss.Web/ClientApp/src/components/reports/CorrectReportsTable.js
+++ src/RX.Nyss.Web/ClientApp/src/components/reports/CorrectReportsTable.js
@@ -145,11 +145,11 @@ export const CorrectReportsTable = ({ isListFetching, isMarkingAsError, markAsEr
               <TableCell style={{ width: '7%' }}>{strings(stringKeys.reports.list.femalesBelowFive)}</TableCell>
               <TableCell style={{ width: '8%' }}>{strings(stringKeys.reports.list.femalesAtLeastFive)}</TableCell>
               {filters.dataCollectorType === DataCollectorType.collectionPoint &&
-                <Fragment>
-                  <TableCell style={{ width: '10%', minWidth: '50px' }}>{strings(stringKeys.reports.list.referredCount)}</TableCell>
-                  <TableCell style={{ width: '10%', minWidth: '50px' }}>{strings(stringKeys.reports.list.deathCount)}</TableCell>
-                  <TableCell style={{ width: '10%', minWidth: '50px' }}>{strings(stringKeys.reports.list.fromOtherVillagesCount)}</TableCell>
-                </Fragment>
+              <Fragment>
+                <TableCell style={{ width: '10%', minWidth: '50px' }}>{strings(stringKeys.reports.list.referredCount)}</TableCell>
+                <TableCell style={{ width: '10%', minWidth: '50px' }}>{strings(stringKeys.reports.list.deathCount)}</TableCell>
+                <TableCell style={{ width: '10%', minWidth: '50px' }}>{strings(stringKeys.reports.list.fromOtherVillagesCount)}</TableCell>
+              </Fragment>
               }
               <TableCell style={{ width: '3%' }} />
             </TableRow>
@@ -158,9 +158,7 @@ export const CorrectReportsTable = ({ isListFetching, isMarkingAsError, markAsEr
             {list.map(row => (
               <TableRow key={row.id} hover>
                 <TableCell>{dayjs(row.dateTime).format('YYYY-MM-DD HH:mm')}</TableCell>
-                <TableCell>
-                  {strings(stringKeys.reports.status[row.status])}
-                </TableCell>
+                <TableCell>{!row.isActivityReport && (strings(stringKeys.reports.status[row.status]))}</TableCell>
                 <TableCell>
                   {row.dataCollectorDisplayName}
                   {!row.isAnonymized && row.dataCollectorDisplayName ? <br /> : ''}
@@ -173,11 +171,11 @@ export const CorrectReportsTable = ({ isListFetching, isMarkingAsError, markAsEr
                 <TableCell>{dashIfEmpty(row.countFemalesBelowFive)}</TableCell>
                 <TableCell>{dashIfEmpty(row.countFemalesAtLeastFive)}</TableCell>
                 {filters.dataCollectorType === DataCollectorType.collectionPoint &&
-                  <Fragment>
-                    <TableCell>{dashIfEmpty(row.referredCount)}</TableCell>
-                    <TableCell>{dashIfEmpty(row.deathCount)}</TableCell>
-                    <TableCell>{dashIfEmpty(row.fromOtherVillagesCount)}</TableCell>
-                  </Fragment>
+                <Fragment>
+                  <TableCell>{dashIfEmpty(row.referredCount)}</TableCell>
+                  <TableCell>{dashIfEmpty(row.deathCount)}</TableCell>
+                  <TableCell>{dashIfEmpty(row.fromOtherVillagesCount)}</TableCell>
+                </Fragment>
                 }
                 <TableCell>
                   <TableRowActions>
