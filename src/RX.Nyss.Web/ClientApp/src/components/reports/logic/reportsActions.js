3ae44a8821b033482794fb97a9b1cd5fc03981ae
Miron Jakubowski <miron.jakubowski@fabres.pl>
2021-10-18 12:45:28 +0200

    Track report export

--- src/RX.Nyss.Web/ClientApp/src/components/reports/logic/reportsActions.js
+++ src/RX.Nyss.Web/ClientApp/src/components/reports/logic/reportsActions.js
@@ -1,101 +1,192 @@
 import { push } from "connected-react-router";
 import {
   OPEN_CORRECT_REPORTS_LIST,
-  OPEN_REPORT_EDITION, EDIT_REPORT,
-  EXPORT_TO_EXCEL, MARK_AS_ERROR, EXPORT_TO_CSV, SEND_REPORT, OPEN_SEND_REPORT, ACCEPT_REPORT, DISMISS_REPORT, OPEN_INCORRECT_REPORTS_LIST, GET_INCORRECT_REPORTS, GET_CORRECT_REPORTS
+  OPEN_REPORT_EDITION,
+  EDIT_REPORT,
+  EXPORT_TO_EXCEL,
+  MARK_AS_ERROR,
+  EXPORT_TO_CSV,
+  SEND_REPORT,
+  OPEN_SEND_REPORT,
+  ACCEPT_REPORT,
+  DISMISS_REPORT,
+  OPEN_INCORRECT_REPORTS_LIST,
+  GET_INCORRECT_REPORTS,
+  GET_CORRECT_REPORTS,
+  TRACK_REPORT_EXPORT,
 } from "./reportsConstants";
 
 export const goToList = (projectId) => push(`/projects/${projectId}/reports`);
-export const goToEditing = (projectId, reportId) => push(`/projects/${projectId}/reports/${reportId}/edit`);
-export const goToAlert = (projectId, alertId) => push(`/projects/${projectId}/alerts/${alertId}/assess`);
+export const goToEditing = (projectId, reportId) =>
+  push(`/projects/${projectId}/reports/${reportId}/edit`);
+export const goToAlert = (projectId, alertId) =>
+  push(`/projects/${projectId}/alerts/${alertId}/assess`);
 
 export const openCorrectReportsList = {
-  invoke: (projectId) => ({ type: OPEN_CORRECT_REPORTS_LIST.INVOKE, projectId }),
+  invoke: (projectId) => ({
+    type: OPEN_CORRECT_REPORTS_LIST.INVOKE,
+    projectId,
+  }),
   request: () => ({ type: OPEN_CORRECT_REPORTS_LIST.REQUEST }),
-  success: (projectId, filtersData) => ({ type: OPEN_CORRECT_REPORTS_LIST.SUCCESS, projectId, filtersData }),
-  failure: (message) => ({ type: OPEN_CORRECT_REPORTS_LIST.FAILURE, message })
+  success: (projectId, filtersData) => ({
+    type: OPEN_CORRECT_REPORTS_LIST.SUCCESS,
+    projectId,
+    filtersData,
+  }),
+  failure: (message) => ({ type: OPEN_CORRECT_REPORTS_LIST.FAILURE, message }),
 };
 
 export const openIncorrectReportsList = {
-  invoke: (projectId) => ({ type: OPEN_INCORRECT_REPORTS_LIST.INVOKE, projectId }),
+  invoke: (projectId) => ({
+    type: OPEN_INCORRECT_REPORTS_LIST.INVOKE,
+    projectId,
+  }),
   request: () => ({ type: OPEN_INCORRECT_REPORTS_LIST.REQUEST }),
-  success: (projectId) => ({ type: OPEN_INCORRECT_REPORTS_LIST.SUCCESS, projectId }),
-  failure: (message) => ({ type: OPEN_INCORRECT_REPORTS_LIST.FAILURE, message })
+  success: (projectId) => ({
+    type: OPEN_INCORRECT_REPORTS_LIST.SUCCESS,
+    projectId,
+  }),
+  failure: (message) => ({
+    type: OPEN_INCORRECT_REPORTS_LIST.FAILURE,
+    message,
+  }),
 };
 
 export const getCorrectList = {
-  invoke: (projectId, pageNumber, filters, sorting) => ({ type: GET_CORRECT_REPORTS.INVOKE, projectId, pageNumber, filters, sorting }),
+  invoke: (projectId, pageNumber, filters, sorting) => ({
+    type: GET_CORRECT_REPORTS.INVOKE,
+    projectId,
+    pageNumber,
+    filters,
+    sorting,
+  }),
   request: () => ({ type: GET_CORRECT_REPORTS.REQUEST }),
-  success: (data, page, rowsPerPage, totalRows, filters, sorting) => ({ type: GET_CORRECT_REPORTS.SUCCESS, data, page, rowsPerPage, totalRows, filters, sorting }),
-  failure: (message) => ({ type: GET_CORRECT_REPORTS.FAILURE, message })
+  success: (data, page, rowsPerPage, totalRows, filters, sorting) => ({
+    type: GET_CORRECT_REPORTS.SUCCESS,
+    data,
+    page,
+    rowsPerPage,
+    totalRows,
+    filters,
+    sorting,
+  }),
+  failure: (message) => ({ type: GET_CORRECT_REPORTS.FAILURE, message }),
 };
 
 export const getIncorrectList = {
-  invoke: (projectId, pageNumber, filters, sorting) => ({ type: GET_INCORRECT_REPORTS.INVOKE, projectId, pageNumber, filters, sorting }),
+  invoke: (projectId, pageNumber, filters, sorting) => ({
+    type: GET_INCORRECT_REPORTS.INVOKE,
+    projectId,
+    pageNumber,
+    filters,
+    sorting,
+  }),
   request: () => ({ type: GET_INCORRECT_REPORTS.REQUEST }),
-  success: (data, page, rowsPerPage, totalRows, filters, sorting) => ({ type: GET_INCORRECT_REPORTS.SUCCESS, data, page, rowsPerPage, totalRows, filters, sorting }),
-  failure: (message) => ({ type: GET_INCORRECT_REPORTS.FAILURE, message })
+  success: (data, page, rowsPerPage, totalRows, filters, sorting) => ({
+    type: GET_INCORRECT_REPORTS.SUCCESS,
+    data,
+    page,
+    rowsPerPage,
+    totalRows,
+    filters,
+    sorting,
+  }),
+  failure: (message) => ({ type: GET_INCORRECT_REPORTS.FAILURE, message }),
 };
 
 export const openEdition = {
-  invoke: (projectId, reportId) => ({ type: OPEN_REPORT_EDITION.INVOKE, projectId, reportId }),
+  invoke: (projectId, reportId) => ({
+    type: OPEN_REPORT_EDITION.INVOKE,
+    projectId,
+    reportId,
+  }),
   request: () => ({ type: OPEN_REPORT_EDITION.REQUEST }),
-  success: (data, healthRisks, dataCollectors) => ({ type: OPEN_REPORT_EDITION.SUCCESS, data, healthRisks, dataCollectors }),
-  failure: (message) => ({ type: OPEN_REPORT_EDITION.FAILURE, message })
+  success: (data, healthRisks, dataCollectors) => ({
+    type: OPEN_REPORT_EDITION.SUCCESS,
+    data,
+    healthRisks,
+    dataCollectors,
+  }),
+  failure: (message) => ({ type: OPEN_REPORT_EDITION.FAILURE, message }),
 };
 
 export const edit = {
-  invoke: (projectId, reportId, data) => ({ type: EDIT_REPORT.INVOKE, projectId, reportId, data }),
+  invoke: (projectId, reportId, data) => ({
+    type: EDIT_REPORT.INVOKE,
+    projectId,
+    reportId,
+    data,
+  }),
   request: () => ({ type: EDIT_REPORT.REQUEST }),
   success: () => ({ type: EDIT_REPORT.SUCCESS }),
-  failure: (message) => ({ type: EDIT_REPORT.FAILURE, message, suppressPopup: true })
+  failure: (message) => ({
+    type: EDIT_REPORT.FAILURE,
+    message,
+    suppressPopup: true,
+  }),
 };
 
 export const exportToExcel = {
-  invoke: (projectId, filters, sorting) => ({ type: EXPORT_TO_EXCEL.INVOKE, projectId, filters, sorting }),
+  invoke: (projectId, filters, sorting) => ({
+    type: EXPORT_TO_EXCEL.INVOKE,
+    projectId,
+    filters,
+    sorting,
+  }),
   request: () => ({ type: EXPORT_TO_EXCEL.REQUEST }),
   success: () => ({ type: EXPORT_TO_EXCEL.SUCCESS }),
-  failure: (message) => ({ type: EXPORT_TO_EXCEL.FAILURE, message })
+  failure: (message) => ({ type: EXPORT_TO_EXCEL.FAILURE, message }),
 };
 
 export const exportToCsv = {
-  invoke: (projectId, filters, sorting) => ({ type: EXPORT_TO_CSV.INVOKE, projectId, filters, sorting }),
+  invoke: (projectId, filters, sorting) => ({
+    type: EXPORT_TO_CSV.INVOKE,
+    projectId,
+    filters,
+    sorting,
+  }),
   request: () => ({ type: EXPORT_TO_CSV.REQUEST }),
   success: () => ({ type: EXPORT_TO_CSV.SUCCESS }),
-  failure: (message) => ({ type: EXPORT_TO_CSV.FAILURE, message })
+  failure: (message) => ({ type: EXPORT_TO_CSV.FAILURE, message }),
 };
 
 export const markAsError = {
   invoke: (reportId) => ({ type: MARK_AS_ERROR.INVOKE, reportId }),
   request: () => ({ type: MARK_AS_ERROR.REQUEST }),
   success: () => ({ type: MARK_AS_ERROR.SUCCESS }),
-  failure: (message) => ({ type: MARK_AS_ERROR.FAILURE, message })
+  failure: (message) => ({ type: MARK_AS_ERROR.FAILURE, message }),
 };
 
 export const openSendReport = {
   invoke: (projectId) => ({ type: OPEN_SEND_REPORT.INVOKE, projectId }),
   request: () => ({ type: OPEN_SEND_REPORT.REQUEST }),
-  success: (dataCollectors, formData) => ({ type: OPEN_SEND_REPORT.SUCCESS, dataCollectors, formData }),
-  failure: (message) => ({ type: OPEN_SEND_REPORT.FAILURE, message })
-}
+  success: (dataCollectors, formData) => ({
+    type: OPEN_SEND_REPORT.SUCCESS,
+    dataCollectors,
+    formData,
+  }),
+  failure: (message) => ({ type: OPEN_SEND_REPORT.FAILURE, message }),
+};
 
 export const sendReport = {
   invoke: (report) => ({ type: SEND_REPORT.INVOKE, report }),
   request: () => ({ type: SEND_REPORT.REQUEST }),
   success: () => ({ type: SEND_REPORT.SUCCESS }),
-  failure: (message) => ({ type: SEND_REPORT.FAILURE, message })
-}
+  failure: (message) => ({ type: SEND_REPORT.FAILURE, message }),
+};
 
 export const acceptReport = {
   invoke: (reportId) => ({ type: ACCEPT_REPORT.INVOKE, reportId }),
   request: () => ({ type: ACCEPT_REPORT.REQUEST }),
   success: () => ({ type: ACCEPT_REPORT.SUCCESS }),
-  failure: (message) => ({ type: ACCEPT_REPORT.FAILURE, message })
-}
+  failure: (message) => ({ type: ACCEPT_REPORT.FAILURE, message }),
+};
 
 export const dismissReport = {
   invoke: (reportId) => ({ type: DISMISS_REPORT.INVOKE, reportId }),
   request: () => ({ type: DISMISS_REPORT.REQUEST }),
   success: () => ({ type: DISMISS_REPORT.SUCCESS }),
-  failure: (message) => ({ type: DISMISS_REPORT.FAILURE, message })
-}
+  failure: (message) => ({ type: DISMISS_REPORT.FAILURE, message }),
+};
+
+export const trackReportExport = (page, fileType, projectId) => ({ type: TRACK_REPORT_EXPORT.INVOKE, page, fileType, projectId });
--- src/RX.Nyss.Web/ClientApp/src/utils/tracking.js
+++ src/RX.Nyss.Web/ClientApp/src/utils/tracking.js
@@ -5,9 +5,11 @@ import { initialState } from "../initialState";
 
 const INIT_TRACKING = action("INIT_TRACKING");
 const TRACK_PAGEVIEW = action("TRACK_PAGEVIEW");
+const TRACK_TRACE = action("TRACK_TRACE");
 
 export const actions = {
   pageView: (alias) => ({ type: TRACK_PAGEVIEW.INVOKE, alias }),
+  trackTrace: (message, properties) => ({ type: TRACK_TRACE.INVOKE, message, properties }),
 };
 
 export function* initTracking() {
@@ -33,9 +35,23 @@ function* trackPage({ alias }) {
   });
 }
 
+export function* trackTrace({ message, properties }) {
+  const user = yield select(state => state.appData.user);
+  const appInsights = yield select(state => state.tracking.appInsights);
+  
+  appInsights.trackTrace({
+    message,
+    properties: {
+      ...properties,
+      userRole: user.roles[0],      
+    },
+  });
+}
+
 export const trackingSagas = () => [
   takeEvery(INIT_TRACKING.INVOKE, initTracking),
   takeEvery(TRACK_PAGEVIEW.INVOKE, trackPage),
+  takeEvery(TRACK_TRACE.INVOKE, trackTrace),
 ];
 
 export function trackingReducer(state = initialState.tracking, action) {
