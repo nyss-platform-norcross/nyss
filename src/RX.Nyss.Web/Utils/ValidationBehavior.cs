4b86accdbedc84bd6bd58dc822f908136781aa15
gardnk <gard.njatun.kroyer@redcross.no>
2021-11-10 14:18:59 +0100

    add mediatR validation behavior

---
+++ src/RX.Nyss.Web/Utils/ValidationBehavior.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+
+namespace RX.Nyss.Web.Utils
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            foreach (var validator in _validators ?? new List<IValidator<TRequest>>())
+            {
+                await validator.ValidateAndThrowAsync(request, cancellationToken: cancellationToken);
+            }
+            return await next();
+        }
+    }
+}
