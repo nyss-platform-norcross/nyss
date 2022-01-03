6b1270142ba26e3a0f59fc0d6e5382a8fd7213fa
gardnk <gard.njatun.kroyer@redcross.no>
2022-01-03 13:22:48 +0100

    remove users name and phone number and add national society name

--- src/RX.Nyss.Web/Features/Feedback/Commands/SendFeedbackCommand.cs
+++ src/RX.Nyss.Web/Features/Feedback/Commands/SendFeedbackCommand.cs
@@ -1,11 +1,14 @@
-﻿using System.Reflection;
+﻿using System.Linq;
+using System.Reflection;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using RX.Nyss.Common.Utils;
 using RX.Nyss.Common.Utils.DataContract;
+using RX.Nyss.Data;
 using RX.Nyss.Web.Configuration;
 using RX.Nyss.Web.Services;
 using RX.Nyss.Web.Services.Authorization;
@@ -29,18 +32,22 @@ namespace RX.Nyss.Web.Features.Feedback.Commands
 
         private readonly IEmailPublisherService _emailPublisherService;
 
+        private readonly INyssContext _nyssContext;
+
         public Handler(
             IAuthorizationService authorizationService,
             IDateTimeProvider dateTimeProvider,
             INyssWebConfig config,
             IHttpContextAccessor httpContextAccessor,
-            IEmailPublisherService emailPublisherService)
+            IEmailPublisherService emailPublisherService,
+            INyssContext nyssContext)
         {
             _authorizationService = authorizationService;
             _dateTimeProvider = dateTimeProvider;
             _config = config;
             _httpContextAccessor = httpContextAccessor;
             _emailPublisherService = emailPublisherService;
+            _nyssContext = nyssContext;
         }
 
         public async Task<Result> Handle(SendFeedbackCommand request, CancellationToken cancellationToken)
@@ -48,11 +55,15 @@ namespace RX.Nyss.Web.Features.Feedback.Commands
             var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
             var currentUser = await _authorizationService.GetCurrentUser();
             var userAgent = _httpContextAccessor.HttpContext?.Request.Headers["User-Agent"].ToString();
+            var nationalSocietyName = await _nyssContext.UserNationalSocieties
+                .Where(uns => uns.User == currentUser)
+                .Select(uns => uns.NationalSociety.Name)
+                .FirstOrDefaultAsync(cancellationToken);
 
-            var messageBody = $@"<p>{currentUser.Name} has sent the following feedback:</p><hr/>
+            var messageBody = $@"<p>A user has sent the following feedback:</p><hr/>
                 <p>{HtmlEncoder.Default.Encode(request.Message)}</p>
                 <p>User's email: {currentUser.EmailAddress}</p>
-                <p>User's phone: {currentUser.PhoneNumber}</p>
+                <p>User's national society: {nationalSocietyName}</p>
                 <p>Browser used: {userAgent}</p>
                 <p>Nyss platform version: {version}</p>
                 <p>Date: {_dateTimeProvider.UtcNow:O}</p>";
