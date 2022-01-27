9171b04e6894bb23c07febea8951f2f8f1331530
gardnk <gard.njatun.kroyer@redcross.no>
2022-01-27 15:40:48 +0100

    use minimal hosting model for web app and report api

--- src/RX.Nyss.ReportApi/Program.cs
+++ src/RX.Nyss.ReportApi/Program.cs
@@ -1,20 +1,14 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using RX.Nyss.ReportApi.Configuration;
 
-namespace RX.Nyss.ReportApi
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.ConfigureDependencies(builder.Configuration);
+var app = builder.Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+app.UseCustomExceptionHandler();
+app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nyss Report API V1"));
+app.MapControllers();
+
+app.Run();
--- src/RX.Nyss.Web/Startup.cs
+++ src/RX.Nyss.Web/Startup.cs
@@ -14,118 +14,114 @@ using Microsoft.Net.Http.Headers;
 using RX.Nyss.Common.Configuration;
 using RX.Nyss.Web.Configuration;
 
-namespace RX.Nyss.Web
+namespace RX.Nyss.Web;
+
+public class Startup
 {
-    public class Startup
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
     {
-        public IConfiguration Configuration { get; }
+        Configuration = configuration;
+    }
 
-        public Startup(IConfiguration configuration)
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        services.ConfigureDependencies(Configuration);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        var config = Configuration.Get<ConfigSingleton>();
+
+        if (!env.IsDevelopment())
         {
-            Configuration = configuration;
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        app.UseCustomExceptionHandler();
+
+        var supportedCultures = config.Languages.Split(",").Select(lang => new CultureInfo(lang)).ToList();
+
+        app.UseRequestLocalization(new RequestLocalizationOptions
         {
-            services.AddDatabaseDeveloperPageExceptionFilter();
+            SupportedCultures = supportedCultures,
+            SupportedUICultures = supportedCultures,
+            RequestCultureProviders = new List<IRequestCultureProvider>
+            {
+                new AcceptLanguageHeaderRequestCultureProvider()
+            }
+        });
 
-            services.ConfigureDependencies(Configuration);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        app.UseHttpsRedirection();
+        app.UseSpaStaticFiles(new StaticFileOptions
         {
-            var config = Configuration.Get<ConfigSingleton>();
-
-            if (env.IsDevelopment())
+            OnPrepareResponse = ctx =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseCustomExceptionHandler();
-
-            var supportedCultures = config.Languages.Split(",").Select(lang => new CultureInfo(lang)).ToList();
-
-            app.UseRequestLocalization(new RequestLocalizationOptions
-            {
-                SupportedCultures = supportedCultures,
-                SupportedUICultures = supportedCultures,
-                RequestCultureProviders = new List<IRequestCultureProvider>
+                if (ctx.Context.Request.Path.StartsWithSegments("/static"))
                 {
-                    new AcceptLanguageHeaderRequestCultureProvider()
+                    ctx.Context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue
+                    {
+                        Public = true,
+                        MaxAge = TimeSpan.FromDays(365)
+                    };
                 }
-            });
+                else
+                {
+                    // don't cache index.html, manifest.json etc
+                    ctx.Context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue
+                    {
+                        Public = true,
+                        MaxAge = TimeSpan.FromDays(0)
+                    };
+                }
+            }
+        });
 
-            app.UseHttpsRedirection();
-            app.UseSpaStaticFiles(new StaticFileOptions
+
+        if (Configuration["Environment"] != NyssEnvironments.Prod && Configuration["Environment"] != NyssEnvironments.Demo)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nyss API V1"));
+        }
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action=Index}/{id?}");
+            endpoints.MapRazorPages();
+        });
+
+        app.UseSpa(spa =>
+        {
+            spa.Options.SourcePath = "ClientApp";
+            spa.Options.DefaultPageStaticFileOptions = new StaticFileOptions
             {
                 OnPrepareResponse = ctx =>
                 {
-                    if (ctx.Context.Request.Path.StartsWithSegments("/static"))
+                    ctx.Context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue
                     {
-                        ctx.Context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue
-                        {
-                            Public = true,
-                            MaxAge = TimeSpan.FromDays(365)
-                        };
-                    }
-                    else
-                    {
-                        // don't cache index.html, manifest.json etc
-                        ctx.Context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue
-                        {
-                            Public = true,
-                            MaxAge = TimeSpan.FromDays(0)
-                        };
-                    }
+                        Public = true,
+                        MaxAge = TimeSpan.FromDays(0)
+                    };
                 }
-            });
+            };
 
-            if (Configuration["Environment"] != NyssEnvironments.Prod && Configuration["Environment"] != NyssEnvironments.Demo)
+            if (env.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nyss API V1"));
+                spa.UseReactDevelopmentServer(npmScript: "start");
             }
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
-            });
-
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "ClientApp";
-                spa.Options.DefaultPageStaticFileOptions = new StaticFileOptions
-                {
-                    OnPrepareResponse = ctx =>
-                    {
-                        ctx.Context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue
-                        {
-                            Public = true,
-                            MaxAge = TimeSpan.FromDays(0)
-                        };
-                    }
-                };
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
-                }
-            });
-        }
+        });
     }
 }
