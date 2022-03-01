899a1255bb8c63701615c4642765377329761b54
gardnk <gard.njatun.kroyer@redcross.no>
2022-03-01 14:39:23 +0100

    use rtl styling for breadcrumb and top menu

--- src/RX.Nyss.Web/ClientApp/src/components/layout/TopMenu.js
+++ src/RX.Nyss.Web/ClientApp/src/components/layout/TopMenu.js
@@ -2,19 +2,23 @@ import styles from './TopMenu.module.scss';
 
 import React from 'react';
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import { Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom'
 
-const TopMenuComponent = ({ topMenu }) => (
-  <Typography className={styles.topMenu}>
-    {topMenu.length !== 0 && topMenu.map((item, index) => (
-      <Link key={`topMenu${index}`} to={item.url} className={item.isActive ? styles.active : ""}>
-        <span>{item.title}</span>
-      </Link>
-    ))}
-  </Typography>
-);
+const TopMenuComponent = ({ topMenu }) => {
+  const directionRtl = useSelector(state => state.appData.direction === 'rtl');
+
+  return (
+    <Typography className={`${styles.topMenu} ${directionRtl ? styles.rtl : ''}`}>
+      {topMenu.length !== 0 && topMenu.map((item, index) => (
+        <Link key={`topMenu${index}`} to={item.url} className={item.isActive ? styles.active : ""}>
+          <span>{item.title}</span>
+        </Link>
+      ))}
+    </Typography>
+  )
+};
 
 
 TopMenuComponent.propTypes = {
