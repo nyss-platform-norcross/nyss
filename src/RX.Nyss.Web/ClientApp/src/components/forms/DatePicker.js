b7489e5744bc65cfb13d7f36d1bb8c06ae997c5a
gardnk <gard.njatun.kroyer@redcross.no>
2022-03-02 13:06:46 +0100

    date picker supports different locales

--- src/RX.Nyss.Web/ClientApp/src/components/forms/DatePicker.js
+++ src/RX.Nyss.Web/ClientApp/src/components/forms/DatePicker.js
@@ -1,24 +1,62 @@
 import styles from "./DatePicker.module.scss"
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from "prop-types";
-import { KeyboardDatePicker } from '@material-ui/pickers';
+import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import format from "date-fns/format";
+import frLocale from "date-fns/locale/fr";
+import arLocale from "date-fns/locale/ar";
+import esLocale from 'date-fns/locale/es';
+import enLocale from 'date-fns/locale/en-GB';
+import DateFnsUtils from "@date-io/date-fns";
+import { useSelector } from "react-redux";
 
-export const DatePicker = ({ label, value, onChange, className, fullWidth }) => (
-  <KeyboardDatePicker
-    className={`${className} ${fullWidth ? '' : styles.datePicker}`}
-    autoOk
-    disableFuture
-    disableToolbar
-    variant="inline"
-    format="YYYY-MM-DD"
-    onChange={onChange}
-    label={label}
-    value={value}
-    InputProps={{ readOnly: true }}
-    InputLabelProps={{ shrink: true }}
-  />
-);
+class LocalizedUtils extends DateFnsUtils {
+  getDatePickerHeaderText(date) {
+    return format(date, "yyyy-MM-dd", { locale: this.locale });
+  }
+}
+
+export const DatePicker = ({ label, value, onChange, className, fullWidth }) => {
+  const userLanguageCode = useSelector(state => state.appData.user.languageCode);
+  const [locale, setLocale] = useState(enLocale);
+
+  useEffect(() => {
+    switch (userLanguageCode) {
+      case 'en':
+        setLocale(enLocale);
+        break;
+      case 'es':
+        setLocale(esLocale);
+        break;
+      case 'fr':
+        setLocale(frLocale);
+        break;
+      case 'ar':
+        setLocale(arLocale);
+        break;
+      default:
+        setLocale(enLocale);
+    }
+  }, [userLanguageCode]);
+
+  return (
+    <MuiPickersUtilsProvider utils={LocalizedUtils} locale={locale}>
+      <KeyboardDatePicker
+        className={`${className} ${fullWidth ? '' : styles.datePicker}`}
+        autoOk
+        disableFuture
+        disableToolbar
+        variant="inline"
+        format="yyyy-MM-dd"
+        onChange={onChange}
+        label={label}
+        value={value}
+        InputProps={{ readOnly: true }}
+        InputLabelProps={{ shrink: true }}
+      />
+    </MuiPickersUtilsProvider>
+  )
+};
 
 DatePicker.propTypes = {
   label: PropTypes.string,
