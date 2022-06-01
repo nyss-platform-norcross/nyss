f00b69047604c98545f4d1dc98b302948edad0eb
gardnk <gard.njatun.kroyer@redcross.no>
2022-06-01 12:27:26 +0200

    show error message when edit fails

--- src/RX.Nyss.Web/ClientApp/src/components/nationalSocietyStructure/logic/nationalSocietyStructureActions.js
+++ src/RX.Nyss.Web/ClientApp/src/components/nationalSocietyStructure/logic/nationalSocietyStructureActions.js
@@ -35,7 +35,7 @@ export const editRegion = {
   invoke: (id, name) => ({ type: EDIT_REGION.INVOKE, id, name }),
   request: (id) => ({ type: EDIT_REGION.REQUEST, id }),
   success: (id, name) => ({ type: EDIT_REGION.SUCCESS, id, name }),
-  failure: (id, message) => ({ type: EDIT_REGION.FAILURE, id, message })
+  failure: (message) => ({ type: EDIT_REGION.FAILURE, message })
 };
 
 export const removeRegion = {
@@ -56,7 +56,7 @@ export const editDistrict = {
   invoke: (id, name) => ({ type: EDIT_DISTRICT.INVOKE, id, name }),
   request: (id) => ({ type: EDIT_DISTRICT.REQUEST, id }),
   success: (id, name) => ({ type: EDIT_DISTRICT.SUCCESS, id, name }),
-  failure: (id, message) => ({ type: EDIT_DISTRICT.FAILURE, id, message })
+  failure: (message) => ({ type: EDIT_DISTRICT.FAILURE, message })
 };
 
 export const removeDistrict = {
@@ -77,7 +77,7 @@ export const editVillage = {
   invoke: (id, name) => ({ type: EDIT_VILLAGE.INVOKE, id, name }),
   request: (id) => ({ type: EDIT_VILLAGE.REQUEST, id }),
   success: (id, name) => ({ type: EDIT_VILLAGE.SUCCESS, id, name }),
-  failure: (id, message) => ({ type: EDIT_VILLAGE.FAILURE, id, message })
+  failure: (message) => ({ type: EDIT_VILLAGE.FAILURE, message })
 };
 
 export const removeVillage = {
@@ -98,7 +98,7 @@ export const editZone = {
   invoke: (id, name) => ({ type: EDIT_ZONE.INVOKE, id, name }),
   request: (id) => ({ type: EDIT_ZONE.REQUEST, id }),
   success: (id, name) => ({ type: EDIT_ZONE.SUCCESS, id, name }),
-  failure: (id, message) => ({ type: EDIT_ZONE.FAILURE, id, message })
+  failure: (message) => ({ type: EDIT_ZONE.FAILURE, message })
 };
 
 export const removeZone = {
