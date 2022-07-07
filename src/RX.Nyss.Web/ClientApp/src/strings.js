5a130c2bc8f2c0a5f349e604966f2b8c62941cfe
Tordar Tømmervik <tordar.tommervik@redcross.no>
2022-07-07 13:50:43 +0200

    fixed stringkeys for user roles

--- src/RX.Nyss.Web/ClientApp/src/strings.js
+++ src/RX.Nyss.Web/ClientApp/src/strings.js
@@ -21,10 +21,10 @@ export const stringKeys = {
     }
   },
   roles: {
-    "TechnicalAdvisor": "role.technicalAdvisor",
+    "TechnicalAdvisor": "role.technicaladvisor",
     "Administrator": "role.administrator",
-    "GlobalCoordinator": "role.globalCoordinator",
-    "DataConsumer": "role.dataConsumer",
+    "GlobalCoordinator": "role.globalcoordinator",
+    "DataConsumer": "role.dataconsumer",
     "Manager": "role.manager",
     "Supervisor": "role.supervisor",
     "Coordinator": "role.coordinator"
