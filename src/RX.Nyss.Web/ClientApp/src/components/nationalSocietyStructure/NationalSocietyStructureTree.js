fc5779875cd543a39fb90f8b8427d317a37039ce
Ian Evangelista <ian-angelo.roman.evangelista@redcross.no>
2023-09-25 13:29:12 +0200

    Restore ns tree

--- src/RX.Nyss.Web/ClientApp/src/components/nationalSocietyStructure/NationalSocietyStructureTree.js
+++ src/RX.Nyss.Web/ClientApp/src/components/nationalSocietyStructure/NationalSocietyStructureTree.js
@@ -1,46 +1,43 @@
-import styles from "./NationalSocietyStructureTree.module.scss";
+import styles from "./NationalSocietyStructureTree.module.scss"
 
-import React, { Fragment } from "react";
-import { Loading } from "../common/loading/Loading";
-import TreeView from "@material-ui/lab/TreeView";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import React, { Fragment } from 'react';
+import { Loading } from '../common/loading/Loading';
+import TreeView from '@material-ui/lab/TreeView';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import { StructureTreeItem } from "./NationalSocietyStructureTreeItem";
 import { strings, stringKeys } from "../../strings";
 import { InlineTextEditor } from "../common/InlineTextEditor/InlineTextEditor";
 import { Icon } from "@material-ui/core";
 import * as roles from "../../authentication/roles";
 import { useSelector } from "react-redux";
-import { NationalSocietyLocationList } from "./NationalSocietyLocationList";
-import { editDistrict, editRegion } from "./logic/nationalSocietyStructureActions";
 
 const AddPanel = ({ placeholder, onSave, rtl }) => {
   return (
     <div className={styles.addPanel}>
-      <Icon className={`${styles.addPanelIcon} ${rtl ? styles.rtl : ""}`}>
-        add
-      </Icon>
-      <InlineTextEditor placeholder={placeholder} onSave={onSave} />
+      <Icon className={`${styles.addPanelIcon} ${rtl ? styles.rtl : ''}`}>add</Icon>
+      <InlineTextEditor
+        placeholder={placeholder}
+        onSave={onSave}
+      />
     </div>
   );
-};
+}
 
 export const NationalSocietyStructureTree = (props) => {
   const { regions, isFetching, nationalSocietyId } = props;
-  const useRtlDirection = useSelector(
-    (state) => state.appData.user.languageCode === "ar"
-  );
+  const useRtlDirection = useSelector(state => state.appData.user.languageCode === 'ar');
 
   if (isFetching || !regions) {
     return <Loading />;
   }
 
   const canModify =
-    !props.nationalSocietyIsArchived &&
-    (!props.nationalSocietyHasCoordinator ||
-      props.callingUserRoles.some(
-        (r) => r === roles.Coordinator || r === roles.Administrator
-      ));
+    !props.nationalSocietyIsArchived
+    && (
+      !props.nationalSocietyHasCoordinator
+      || props.callingUserRoles.some(r => r === roles.Coordinator || r === roles.Administrator)
+    );
 
   const renderRegions = (regions) => {
     if (!regions.length && !canModify) {
@@ -49,7 +46,7 @@ export const NationalSocietyStructureTree = (props) => {
 
     return (
       <Fragment>
-        {regions.map((region) => (
+        {regions.map(region => (
           <StructureTreeItem
             key={`region_${region.id}`}
             itemKey="region"
@@ -64,20 +61,17 @@ export const NationalSocietyStructureTree = (props) => {
         ))}
         {canModify && (
           <AddPanel
-            placeholder={strings(
-              stringKeys.nationalSociety.structure.addRegion,
-              true
-            )}
-            onSave={(name) => props.createRegion(nationalSocietyId, name)}
+            placeholder={strings(stringKeys.nationalSociety.structure.addRegion, true)}
+            onSave={name => props.createRegion(nationalSocietyId, name)}
             rtl={useRtlDirection}
           />
         )}
       </Fragment>
     );
-  };
+  }
 
   const renderDistricts = (regionId) => {
-    const list = props.districts.filter((d) => d.regionId === regionId);
+    const list = props.districts.filter(d => d.regionId === regionId);
 
     if (!list.length && !canModify) {
       return null;
@@ -85,7 +79,7 @@ export const NationalSocietyStructureTree = (props) => {
 
     return (
       <Fragment>
-        {list.map((district) => (
+        {list.map(district => (
           <StructureTreeItem
             key={`district_${district.id}`}
             itemKey="district"
@@ -100,20 +94,17 @@ export const NationalSocietyStructureTree = (props) => {
         ))}
         {canModify && (
           <AddPanel
-            placeholder={strings(
-              stringKeys.nationalSociety.structure.addDistrict,
-              true
-            )}
-            onSave={(name) => props.createDistrict(regionId, name)}
+            placeholder={strings(stringKeys.nationalSociety.structure.addDistrict, true)}
+            onSave={name => props.createDistrict(regionId, name)}
             rtl={useRtlDirection}
           />
         )}
       </Fragment>
     );
-  };
+  }
 
   const renderVillages = (districtId) => {
-    const list = props.villages.filter((d) => d.districtId === districtId);
+    const list = props.villages.filter(d => d.districtId === districtId);
 
     if (!list.length && !canModify) {
       return null;
@@ -121,7 +112,7 @@ export const NationalSocietyStructureTree = (props) => {
 
     return (
       <Fragment>
-        {list.map((village) => (
+        {list.map(village => (
           <StructureTreeItem
             key={`village_${village.id}`}
             itemKey="village"
@@ -136,20 +127,17 @@ export const NationalSocietyStructureTree = (props) => {
         ))}
         {canModify && (
           <AddPanel
-            placeholder={strings(
-              stringKeys.nationalSociety.structure.addVillage,
-              true
-            )}
-            onSave={(name) => props.createVillage(districtId, name)}
+            placeholder={strings(stringKeys.nationalSociety.structure.addVillage, true)}
+            onSave={name => props.createVillage(districtId, name)}
             rtl={useRtlDirection}
           />
         )}
       </Fragment>
     );
-  };
+  }
 
   const renderZones = (villageId) => {
-    const list = props.zones.filter((d) => d.villageId === villageId);
+    const list = props.zones.filter(d => d.villageId === villageId);
 
     if (!list.length && !canModify) {
       return null;
@@ -157,7 +145,7 @@ export const NationalSocietyStructureTree = (props) => {
 
     return (
       <Fragment>
-        {list.map((zone) => (
+        {list.map(zone => (
           <StructureTreeItem
             key={`zone_${zone.id}`}
             itemKey="zone"
@@ -170,73 +158,24 @@ export const NationalSocietyStructureTree = (props) => {
         ))}
         {canModify && (
           <AddPanel
-            placeholder={strings(
-              stringKeys.nationalSociety.structure.addZone,
-              true
-            )}
-            onSave={(name) => props.createZone(villageId, name)}
+            placeholder={strings(stringKeys.nationalSociety.structure.addZone, true)}
+            onSave={name => props.createZone(villageId, name)}
             rtl={useRtlDirection}
           />
         )}
       </Fragment>
     );
-  };
-
-  const manageLocation = {
-    region: {
-      create: props.createRegion,
-      edit: props.editRegion,
-      remove: props.removeRegion,
-      nextLocationType: "district",
-      nextLocations: (location) => props.districts.filter(
-        (district) => district.regionId === location.id
-      )
-    },
-    district: {
-      create: props.createDistrict,
-      edit: props.editDistrict,
-      remove: props.removeDistrict,
-      nextLocationType: "village",
-      nextLocations: (location) => props.villages.filter(
-        (village) => village.districtId === location.id
-      )
-    },
-    village: {
-      create: props.createVillage,
-      edit: props.editVillage,
-      remove: props.removeVillage,
-      nextLocationType: "zone",
-      nextLocations: (location) => props.zones.filter(
-        (zone) => zone.villageId === location.id
-      )
-    },
-    zone: {
-      create: props.createZone,
-      edit: props.editZone,
-      remove: props.removeZone,
-      nextLocationType: null,
-      nextLocations: () => null
-    }
   }
 
   return (
-    <NationalSocietyLocationList
-      locations={props.regions}
-      locationType="region"
-      activeParentLocationId={nationalSocietyId}
-      manageLocation={manageLocation}
-    />
+    <TreeView
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpandIcon={<ChevronRightIcon />}
+      className={styles.tree}
+      expanded={props.expandedItems}
+      onNodeToggle={(e, items) => props.updateExpandedItems(items)}
+    >
+      {renderRegions(regions)}
+    </TreeView>
   );
-
-  // return (
-  //   <TreeView
-  //     defaultCollapseIcon={<ExpandMoreIcon />}
-  //     defaultExpandIcon={<ChevronRightIcon />}
-  //     className={styles.tree}
-  //     expanded={props.expandedItems}
-  //     onNodeToggle={(e, items) => props.updateExpandedItems(items)}
-  //   >
-  //     {renderRegions(regions)}
-  //   </TreeView>
-  // );
-};
+}
