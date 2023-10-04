160a5180fb72fb13d4f156c34e7fd2924033104a
Sondre Kanstad <sondre.kanstad@redcross.no>
2023-10-04 15:01:36 +0200

    Add user roles to feedback message email response

--- src/RX.Nyss.Web/Features/Feedback/Commands/SendFeedbackCommand.cs
+++ src/RX.Nyss.Web/Features/Feedback/Commands/SendFeedbackCommand.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RX.Nyss.Common.Utils;
 using RX.Nyss.Common.Utils.DataContract;
@@ -24,6 +25,8 @@ namespace RX.Nyss.Web.Features.Feedback.Commands
     {
         private readonly IAuthorizationService _authorizationService;
 
+        private readonly UserManager<IdentityUser> _userManager;
+
         private readonly IDateTimeProvider _dateTimeProvider;
 
         private readonly INyssWebConfig _config;
@@ -36,6 +39,7 @@ namespace RX.Nyss.Web.Features.Feedback.Commands
 
         public Handler(
             IAuthorizationService authorizationService,
+            UserManager<IdentityUser> userManager,
             IDateTimeProvider dateTimeProvider,
             INyssWebConfig config,
             IHttpContextAccessor httpContextAccessor,
@@ -43,6 +47,7 @@ namespace RX.Nyss.Web.Features.Feedback.Commands
             INyssContext nyssContext)
         {
             _authorizationService = authorizationService;
+            _userManager = userManager;
             _dateTimeProvider = dateTimeProvider;
             _config = config;
             _httpContextAccessor = httpContextAccessor;
@@ -54,6 +59,8 @@ namespace RX.Nyss.Web.Features.Feedback.Commands
         {
             var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
             var currentUser = await _authorizationService.GetCurrentUser();
+            var currentIdentityUser = await _userManager.FindByIdAsync(currentUser.IdentityUserId);
+            var roles = await _userManager.GetRolesAsync(currentIdentityUser);
             var userAgent = _httpContextAccessor.HttpContext?.Request.Headers["User-Agent"].ToString();
             var nationalSocietyName = await _nyssContext.UserNationalSocieties
                 .Where(uns => uns.User == currentUser)
@@ -64,6 +71,7 @@ namespace RX.Nyss.Web.Features.Feedback.Commands
                 <p>{HtmlEncoder.Default.Encode(request.Message)}</p>
                 <p>User's email: {currentUser.EmailAddress}</p>
                 <p>User's national society: {nationalSocietyName}</p>
+                <p>User's roles: {string.Join(", ", roles)}</p>
                 <p>Browser used: {userAgent}</p>
                 <p>Nyss platform version: {version}</p>
                 <p>Date: {_dateTimeProvider.UtcNow:O}</p>";
