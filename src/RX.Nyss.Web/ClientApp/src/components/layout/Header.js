b7787fb537dc1613251dcaaf05e67cc7cba3650c
Sondre Kanstad <sondre.kanstad@redcross.no>
2023-10-19 15:45:34 +0200

    Add small logo to supervisor header

--- src/RX.Nyss.Web/ClientApp/src/components/layout/Header.js
+++ src/RX.Nyss.Web/ClientApp/src/components/layout/Header.js
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom'
 import React from 'react';
 import { connect, useSelector } from "react-redux";
 import { UserStatus } from './UserStatus';
-import { Icon } from '@material-ui/core';
+import { Icon, useTheme, useMediaQuery } from '@material-ui/core';
 import { toggleSideMenu } from '../app/logic/appActions';
 
 const HeaderComponent = ({ sideMenuOpen, toggleSideMenu, directionRtl, isSupervisor }) =>{
   const userLanguageCode = useSelector(state => state.appData.user.languageCode);
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+
   return (
     <div className={styles.header}>
       {!isSupervisor && (
@@ -19,7 +22,8 @@ const HeaderComponent = ({ sideMenuOpen, toggleSideMenu, directionRtl, isSupervi
       {isSupervisor && (
           <div className={styles.supervisorHeader}>
             <Link to="/" className={userLanguageCode !== 'ar' ? styles.logo : styles.logoDirectionRightToLeft}>
-              <img src="/images/logo.svg" alt="Nyss logo"/>
+              <img src={isSmallScreen ? "/images/logo-small.svg?cache=" + new Date().getTime()
+        : "/images/logo.svg?cache=" + new Date().getTime()} alt="Nyss logo"/>
             </Link>
             <UserStatus />
           </div>
